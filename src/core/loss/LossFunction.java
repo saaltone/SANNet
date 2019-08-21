c7161c0d7a402f196023508d678adb90e116641f
saaltone <simo.aaltonen@kolumbus.fi>
2019-08-21 20:52:18 +0300

    Added new loss functions.

--- src/core/loss/LossFunction.java
+++ src/core/loss/LossFunction.java
@@ -48,6 +48,12 @@ public class LossFunction implements Serializable {
     private double huber_delta = 1;
 
     /**
+     * Margin value for Hinge loss.
+     *
+     */
+    private double hinge_margin = 1;
+
+    /**
      * Constructor for loss function.
      *
      * @param functionType type of loss function.
@@ -71,12 +77,20 @@ public class LossFunction implements Serializable {
         this.functionType = functionType;
         switch(functionType) {
             case MEAN_SQUARED_ERROR:
-                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> 0.5 * Math.pow(pred - target, 2);
-                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> (pred - target);
+                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> 0.5 * Math.pow(target - pred, 2);
+                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> pred - target;
+                break;
+            case MEAN_SQUARED_LOGARITHMIC_ERROR:
+                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> Math.pow(Math.log(target + 1 > 0 ? target + 1 : 10E-8) - Math.log(pred + 1 > 0 ? pred + 1 : 10E-8), 2);
+                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> -2 * (Math.log(target + 1 > 0 ? target + 1 : 10E-8) - Math.log(pred + 1 > 0 ? pred + 1 : 10E-8)) / (target + 1 != 0 ? target + 1 : Double.MAX_VALUE);
                 break;
             case MEAN_ABSOLUTE_ERROR:
-                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> Math.abs(pred - target);
-                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> Math.signum(pred - target);
+                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> Math.abs(target - pred);
+                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> -Math.signum(target - pred);
+                break;
+            case MEAN_ABSOLUTE_PERCENTAGE_ERROR:
+                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> target != 0 ? 100 * Math.abs((pred - target) / target) : Double.MAX_VALUE;
+                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> 100 * (pred - target) / ((target != 0 && (pred - target) != 0) ? Math.abs(target) * Math.abs(pred - target) : Double.MAX_VALUE);
                 break;
             case CROSS_ENTROPY:
                 function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> -(target * Math.log(pred > 0 ? pred : 10E-8));
@@ -93,6 +107,21 @@ public class LossFunction implements Serializable {
             case POISSON:
                 function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> pred - target * Math.log((pred > 0 ? pred : 10E-8));
                 derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> 1 - target / (pred > 0 ? pred : 10E-8);
+                break;
+            case HINGE:
+                if (params != null) {
+                    HashMap<String, DynamicParam.ParamType> paramDefs = new HashMap<>();
+                    paramDefs.put("margin", DynamicParam.ParamType.DOUBLE);
+                    DynamicParam dParams = new DynamicParam(params, paramDefs);
+                    if (dParams.hasParam("margin")) hinge_margin = dParams.getValueAsDouble("margin");
+                }
+                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> hinge_margin - target * pred <= 0 ? 0 : hinge_margin - target * pred;
+                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> hinge_margin - target * pred <= 0 ? 0 : - target;
+                break;
+            case SQUARED_HINGE:
+                function = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> 1 - target * pred <= 0 ? 0 : Math.pow(1 - target * pred, 2);
+                derivative = (Matrix.MatrixBiOperation & Serializable) (target, pred) -> 1 - target * pred <= 0 ? 0 : - 2 * target * (1 - target * pred);
+                break;
             case HUBER:
                 if (params != null) {
                     HashMap<String, DynamicParam.ParamType> paramDefs = new HashMap<>();
--- src/core/loss/LossFunctionType.java
+++ src/core/loss/LossFunctionType.java
@@ -10,21 +10,29 @@ package core.loss;
  * Loss function types.<br>
  * Currently supported types are:
  *     MEAN_SQUARED_ERROR,
+ *     MEAN_SQUARED_LOGARITHMIC_ERROR,
  *     MEAN_ABSOLUTE_ERROR,
+ *     MEAN_ABSOLUTE_PERCENTAGE_ERROR,
  *     CROSS_ENTROPY,
  *     KULLBACK_LEIBLER,
  *     NEGATIVE_LOG_LIKELIHOOD,
  *     POISSON,
+ *     HINGE,
+ *     SQUARED_HINGE,
  *     HUBER
  *
  */
 public enum LossFunctionType {
     MEAN_SQUARED_ERROR,
+    MEAN_SQUARED_LOGARITHMIC_ERROR,
     MEAN_ABSOLUTE_ERROR,
+    MEAN_ABSOLUTE_PERCENTAGE_ERROR,
     CROSS_ENTROPY,
     KULLBACK_LEIBLER,
     NEGATIVE_LOG_LIKELIHOOD,
     POISSON,
+    HINGE,
+    SQUARED_HINGE,
     HUBER
 
 }
