ef3b5f0c8c4a3d4b85df42cffb382f6a460d6218
saaltone <simo.aaltonen@kolumbus.fi>
2020-06-17 23:33:01 +0300

    Added interface to include function calls to define and create new procedure.

---
+++ src/utils/procedure/ForwardProcedure.java
@@ -0,0 +1,29 @@
+package utils.procedure;
+
+import utils.matrix.MMatrix;
+import utils.matrix.MatrixException;
+
+/**
+ * Defines interface for forward procedure.
+ *
+ */
+public interface ForwardProcedure {
+
+    /**
+     * Returns input matrix for procedure construction.
+     *
+     * @param resetPreviousInput if true resets previous input.
+     * @return input matrix for procedure construction.
+     * @throws MatrixException throws exception if matrix operation fails.
+     */
+    MMatrix getInputMatrices(boolean resetPreviousInput) throws MatrixException;
+
+    /**
+     * Builds forward procedure and implicitly builds backward procedure.
+     *
+     * @return output of forward procedure.
+     * @throws MatrixException throws exception if matrix operation fails.
+     */
+    MMatrix getForwardProcedure() throws MatrixException;
+
+}
