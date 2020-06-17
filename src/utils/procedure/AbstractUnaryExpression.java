b37621c945a45fd07b6e66f7abad258679c6815e
saaltone <simo.aaltonen@kolumbus.fi>
2020-06-18 00:03:36 +0300

    Minor updates.

--- src/utils/procedure/AbstractUnaryExpression.java
+++ src/utils/procedure/AbstractUnaryExpression.java
@@ -18,12 +18,12 @@ public abstract class AbstractUnaryExpression extends AbstractExpression {
      * Constructor for unary expression.
      *
      * @param expressionID expression ID
-     * @param arg1 first argument of expression.
+     * @param argument1 first argument of expression.
      * @param result result of expression.
      * @throws MatrixException throws exception if expression arguments are not defined.
      */
-    AbstractUnaryExpression(int expressionID, Node arg1, Node result) throws MatrixException {
-        super(expressionID, arg1, result);
+    AbstractUnaryExpression(int expressionID, Node argument1, Node result) throws MatrixException {
+        super(expressionID, argument1, result);
     }
 
 }
