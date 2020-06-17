cfd67de39772c9d1b60bc2c60e7509ef6a8e2c55
saaltone <simo.aaltonen@kolumbus.fi>
2020-06-17 23:10:10 +0300

    Minor updates.

--- src/core/layer/LayerFactory.java
+++ src/core/layer/LayerFactory.java
@@ -12,7 +12,8 @@ import core.layer.convolutional.*;
 import core.layer.feedforward.*;
 import core.layer.recurrent.*;
 import utils.DynamicParamException;
-import utils.matrix.Init;
+import utils.matrix.Initialization;
+import utils.matrix.MatrixException;
 
 /**
  * Factory class to construct neural network layers.
@@ -21,37 +22,38 @@ import utils.matrix.Init;
 public class LayerFactory {
 
     /**
-     * Creates neural network layer.
+     * Creates neural network execution layer.
      *
+     * @param layerIndex layer Index.
      * @param layerType type of layer.
-     * @param abstractLayer reference to abstract layer.
      * @param activationFunction activation function for layer.
-     * @param intialization initialization function for layer.
+     * @param initialization initialization function for layer.
      * @param params parameters for layer.
      * @return created neural network layer instance
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
      * @throws NeuralNetworkException throws exception setting of activation function fails.
+     * @throws MatrixException throws exception if custom function is attempted to be created with this constructor.
      */
-    public static Layer create(LayerType layerType, AbstractLayer abstractLayer, ActivationFunction activationFunction, Init intialization, String params) throws DynamicParamException, NeuralNetworkException {
+    public static AbstractExecutionLayer create(int layerIndex, LayerType layerType, ActivationFunction activationFunction, Initialization initialization, String params) throws DynamicParamException, NeuralNetworkException, MatrixException {
         switch (layerType) {
             case FEEDFORWARD:
-                return new FeedforwardLayer(abstractLayer, activationFunction, intialization, params);
+                return new FeedforwardLayer(layerIndex, activationFunction, initialization, params);
             case RECURRENT:
-                return new RecurrentLayer(abstractLayer, activationFunction, intialization, params);
+                return new RecurrentLayer(layerIndex, activationFunction, initialization, params);
             case LSTM:
-                return new LSTMLayer(abstractLayer, activationFunction, intialization, params);
+                return new LSTMLayer(layerIndex, initialization, params);
             case PEEPHOLELSTM:
-                return new PeepholeLSTMLayer(abstractLayer, activationFunction, intialization, params);
+                return new PeepholeLSTMLayer(layerIndex, initialization, params);
             case GRAVESLSTM:
-                return new GravesLSTMLayer(abstractLayer, activationFunction, intialization, params);
+                return new GravesLSTMLayer(layerIndex, initialization, params);
             case GRU:
-                return new GRULayer(abstractLayer, activationFunction, intialization, params);
+                return new GRULayer(layerIndex, initialization, params);
             case MINGRU:
-                return new MinGRULayer(abstractLayer, activationFunction, intialization, params);
+                return new MinGRULayer(layerIndex, initialization, params);
             case CONVOLUTIONAL:
-                return new ConvolutionalLayer(abstractLayer, activationFunction, intialization, params);
+                return new ConvolutionalLayer(layerIndex, activationFunction, initialization, params);
             case POOLING:
-                return new PoolingLayer(abstractLayer, activationFunction, intialization, params);
+                return new PoolingLayer(layerIndex, initialization, params);
         }
         return null;
     }
@@ -59,40 +61,40 @@ public class LayerFactory {
     /**
      * Returns type of a given layer.
      *
-     * @param layer given layer.
+     * @param neuralNetworkLayer given layer.
      * @return type of a layer.
      * @throws NeuralNetworkException throws exception if layer is of an unknown type.
      */
-    public static LayerType getLayerType(Layer layer) throws NeuralNetworkException {
-        if (layer instanceof FeedforwardLayer) return LayerType.FEEDFORWARD;
-        if (layer instanceof RecurrentLayer) return LayerType.RECURRENT;
-        if (layer instanceof LSTMLayer) return LayerType.LSTM;
-        if (layer instanceof PeepholeLSTMLayer) return LayerType.PEEPHOLELSTM;
-        if (layer instanceof GravesLSTMLayer) return LayerType.GRAVESLSTM;
-        if (layer instanceof GRULayer) return LayerType.GRU;
-        if (layer instanceof MinGRULayer) return LayerType.MINGRU;
-        if (layer instanceof ConvolutionalLayer) return LayerType.CONVOLUTIONAL;
-        if (layer instanceof PoolingLayer) return LayerType.POOLING;
+    public static LayerType getLayerType(NeuralNetworkLayer neuralNetworkLayer) throws NeuralNetworkException {
+        if (neuralNetworkLayer instanceof FeedforwardLayer) return LayerType.FEEDFORWARD;
+        if (neuralNetworkLayer instanceof RecurrentLayer) return LayerType.RECURRENT;
+        if (neuralNetworkLayer instanceof LSTMLayer) return LayerType.LSTM;
+        if (neuralNetworkLayer instanceof PeepholeLSTMLayer) return LayerType.PEEPHOLELSTM;
+        if (neuralNetworkLayer instanceof GravesLSTMLayer) return LayerType.GRAVESLSTM;
+        if (neuralNetworkLayer instanceof GRULayer) return LayerType.GRU;
+        if (neuralNetworkLayer instanceof MinGRULayer) return LayerType.MINGRU;
+        if (neuralNetworkLayer instanceof ConvolutionalLayer) return LayerType.CONVOLUTIONAL;
+        if (neuralNetworkLayer instanceof PoolingLayer) return LayerType.POOLING;
         throw new NeuralNetworkException("Unknown layer type");
     }
 
     /**
      * Returns type of layer as string.
      *
-     * @param layer given layer.
+     * @param neuralNetworkLayer given layer.
      * @return type of a layer as string.
      * @throws NeuralNetworkException throws exception if layer is of an unknown type.
      */
-    public static String getLayerTypeByName(Layer layer) throws NeuralNetworkException {
-        if (layer instanceof FeedforwardLayer) return "FEEDFORWARD";
-        if (layer instanceof RecurrentLayer) return "RECURRENT";
-        if (layer instanceof LSTMLayer) return "LSTM";
-        if (layer instanceof PeepholeLSTMLayer) return "PEEPHOLELSTM";
-        if (layer instanceof GravesLSTMLayer) return "GRAVESLSTM";
-        if (layer instanceof GRULayer) return "GRU";
-        if (layer instanceof MinGRULayer) return "MINGRU";
-        if (layer instanceof ConvolutionalLayer) return "CONVOLUTIONAL";
-        if (layer instanceof PoolingLayer) return "POOLING";
+    public static String getLayerTypeByName(NeuralNetworkLayer neuralNetworkLayer) throws NeuralNetworkException {
+        if (neuralNetworkLayer instanceof FeedforwardLayer) return "FEEDFORWARD";
+        if (neuralNetworkLayer instanceof RecurrentLayer) return "RECURRENT";
+        if (neuralNetworkLayer instanceof LSTMLayer) return "LSTM";
+        if (neuralNetworkLayer instanceof PeepholeLSTMLayer) return "PEEPHOLELSTM";
+        if (neuralNetworkLayer instanceof GravesLSTMLayer) return "GRAVESLSTM";
+        if (neuralNetworkLayer instanceof GRULayer) return "GRU";
+        if (neuralNetworkLayer instanceof MinGRULayer) return "MINGRU";
+        if (neuralNetworkLayer instanceof ConvolutionalLayer) return "CONVOLUTIONAL";
+        if (neuralNetworkLayer instanceof PoolingLayer) return "POOLING";
         throw new NeuralNetworkException("Unknown layer type");
     }
 
