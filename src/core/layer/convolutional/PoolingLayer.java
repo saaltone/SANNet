312e9a7343472d452a7615fcf95e9a321593c973
saaltone <simo.aaltonen@kolumbus.fi>
2020-06-17 23:37:35 +0300

    Minor updates plus added class printing.

--- src/core/layer/convolutional/PoolingLayer.java
+++ src/core/layer/convolutional/PoolingLayer.java
@@ -7,18 +7,11 @@
 package core.layer.convolutional;
 
 import core.NeuralNetworkException;
-import core.activation.ActivationFunction;
 import core.layer.AbstractExecutionLayer;
-import core.layer.AbstractLayer;
-import core.normalization.Normalization;
 import utils.*;
-import utils.matrix.DMatrix;
-import utils.matrix.Init;
-import utils.matrix.Matrix;
-import utils.matrix.MatrixException;
+import utils.matrix.*;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  * Defines class for pooling layer that executes either average or max pooling.<br>
@@ -34,31 +27,31 @@ public class PoolingLayer extends AbstractExecutionLayer {
      * Defines width of incoming image.
      *
      */
-    private int widthIn;
+    private int previousLayerWidth;
 
     /**
      * Defines height of incoming image.
      *
      */
-    private int heightIn;
+    private int previousLayerHeight;
 
     /**
      * Defines height of incoming image.
      *
      */
-    private int depthIn;
+    private int previousLayerDepth;
 
     /**
      * Defines width of outgoing image.
      *
      */
-    private int widthOut;
+    private int layerWidth;
 
     /**
      * Defines height of outgoing image.
      *
      */
-    private int heightOut;
+    private int layerHeight;
 
     /**
      * Step size of pooling operation.
@@ -76,26 +69,26 @@ public class PoolingLayer extends AbstractExecutionLayer {
      * If true executes average pooling otherwise executes max pooling.
      *
      */
-    private boolean avgPool = false;
+    private boolean averagePool = false;
 
     /**
      * Input matrices for procedure construction.
      *
      */
-    private Sample inputs;
+    private MMatrix inputs;
 
     /**
      * Constructor for pooling layer.
      *
-     * @param parent reference to parent layer.
-     * @param activation activation function used (not relevant for pooling layer).
-     * @param initialization intialization function for weight maps (not relevant for pooling layer).
+     * @param layerIndex layer Index.
+     * @param initialization initialization function for weight maps (not relevant for pooling layer).
      * @param params parameters for pooling layer.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
      * @throws NeuralNetworkException throws exception setting of activation function fails.
      */
-    public PoolingLayer(AbstractLayer parent, ActivationFunction activation, Init initialization, String params) throws DynamicParamException, NeuralNetworkException {
-        super (parent, activation, initialization, params);
+    public PoolingLayer(int layerIndex, Initialization initialization, String params) throws DynamicParamException, NeuralNetworkException {
+        super (layerIndex, initialization, params);
+        setParams(new DynamicParam(params, getParamDefs()));
     }
 
     /**
@@ -104,7 +97,7 @@ public class PoolingLayer extends AbstractExecutionLayer {
      * @return parameters used for pooling layer.
      */
     public HashMap<String, DynamicParam.ParamType> getParamDefs() {
-        HashMap<String, DynamicParam.ParamType> paramDefs = new HashMap<>();
+        HashMap<String, DynamicParam.ParamType> paramDefs = new HashMap<>(super.getParamDefs());
         paramDefs.put("poolSize", DynamicParam.ParamType.INT);
         paramDefs.put("stride", DynamicParam.ParamType.INT);
         paramDefs.put("avgPool", DynamicParam.ParamType.BOOLEAN);
@@ -121,11 +114,13 @@ public class PoolingLayer extends AbstractExecutionLayer {
      *
      * @param params parameters used for pooling layer.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws NeuralNetworkException throws exception if minimum layer dimensions are not met.
      */
-    public void setParams(DynamicParam params) throws DynamicParamException {
+    public void setParams(DynamicParam params) throws DynamicParamException, NeuralNetworkException {
+        super.setParams(params);
         if (params.hasParam("poolSize")) poolSize = params.getValueAsInteger("poolSize");
         if (params.hasParam("stride")) stride = params.getValueAsInteger("stride");
-        if (params.hasParam("avgPool")) avgPool = params.getValueAsBoolean("avgPool");
+        if (params.hasParam("avgPool")) averagePool = params.getValueAsBoolean("avgPool");
     }
 
     /**
@@ -149,54 +144,46 @@ public class PoolingLayer extends AbstractExecutionLayer {
      * @throws NeuralNetworkException thrown if initialization of layer fails.
      */
     public void initialize() throws NeuralNetworkException {
-        widthIn = parent.getBackward().getPLayerWidth();
-        heightIn = parent.getBackward().getPLayerHeight();
-        depthIn = parent.getBackward().getPLayerDepth();
+        previousLayerWidth = getPreviousLayerWidth();
+        previousLayerHeight = getPreviousLayerHeight();
+        previousLayerDepth = getPreviousLayerDepth();
 
-        if ((widthIn - poolSize) % stride != 0)  throw new NeuralNetworkException("Pooling layer widthIn: " + widthIn + " - poolSize: " + poolSize + " must be divisible by stride: " + stride);
-        if ((heightIn - poolSize) % stride != 0)  throw new NeuralNetworkException("Pooling layer heigthIn: " + heightIn + " - poolSize: " + poolSize + " must be divisible by stride: " + stride);
+        if ((previousLayerWidth - poolSize) % stride != 0)  throw new NeuralNetworkException("Pooling layer widthIn: " + previousLayerWidth + " - poolSize: " + poolSize + " must be divisible by stride: " + stride);
+        if ((previousLayerHeight - poolSize) % stride != 0)  throw new NeuralNetworkException("Pooling layer heightIn: " + previousLayerHeight + " - poolSize: " + poolSize + " must be divisible by stride: " + stride);
 
-        widthOut = ((widthIn - poolSize) / stride) + 1;
-        heightOut = ((heightIn - poolSize) / stride) + 1;
+        layerWidth = ((previousLayerWidth - poolSize) / stride) + 1;
+        layerHeight = ((previousLayerHeight - poolSize) / stride) + 1;
 
-        if (widthOut < 1) throw new NeuralNetworkException("Pooling layer width cannot be less than 1: " + widthOut);
-        if (heightOut < 1) throw new NeuralNetworkException("Pooling layer heigth cannot be less than 1: " + heightOut);
-        if (depthIn < 1) throw new NeuralNetworkException("Pooling layer depth cannot be less than 1: " + depthIn);
+        if (layerWidth < 1) throw new NeuralNetworkException("Pooling layer width cannot be less than 1: " + layerWidth);
+        if (layerHeight < 1) throw new NeuralNetworkException("Pooling layer height cannot be less than 1: " + layerHeight);
+        if (previousLayerDepth < 1) throw new NeuralNetworkException("Pooling layer depth cannot be less than 1: " + previousLayerDepth);
 
-        parent.setWidth(widthOut);
-        parent.setHeight(heightOut);
-        parent.setDepth(depthIn);
-    }
-
-    /**
-     * Resets input.
-     *
-     * @param resetPreviousInput if true resets also previous input.
-     * @throws MatrixException throws exception if matrix operation fails.
-     */
-    protected void resetInput(boolean resetPreviousInput) throws MatrixException {
-        inputs = new Sample(depthIn);
-        for (int index = 0; index < depthIn; index++) inputs.put(index, new DMatrix(widthIn, heightIn));
+        setLayerWidth(layerWidth);
+        setLayerHeight(layerHeight);
+        setLayerDepth(previousLayerDepth);
     }
 
     /**
      * Returns input matrix for procedure construction.
      *
+     * @param resetPreviousInput if true resets also previous input.
      * @return input matrix for procedure construction.
+     * @throws MatrixException throws exception if matrix operation fails.
      */
-    protected Sample getInputMatrices() {
+    public MMatrix getInputMatrices(boolean resetPreviousInput) throws MatrixException {
+        inputs = new MMatrix(previousLayerDepth, "Inputs");
+        for (int index = 0; index < previousLayerDepth; index++) inputs.put(index, new DMatrix(previousLayerWidth, previousLayerHeight, "Input" + index));
         return inputs;
     }
 
     /**
      * Builds forward procedure and implicitly builds backward procedure.
      *
-     * @param normalizers normalizers for layer normalization.
      * @return output of forward procedure.
      * @throws MatrixException throws exception if matrix operation fails.
      */
-    protected Sample getForwardProcedure(HashSet<Normalization> normalizers) throws MatrixException {
-        Sample outputs = new Sample(depthIn);
+    public MMatrix getForwardProcedure() throws MatrixException {
+        MMatrix outputs = new MMatrix(previousLayerDepth, "Outputs");
         Matrix output;
 
         for (int channelIndex = 0; channelIndex < inputs.size(); channelIndex++) {
@@ -205,13 +192,26 @@ public class PoolingLayer extends AbstractExecutionLayer {
             input.setStride(stride);
             input.setPoolSize(poolSize);
 
-            outputs.put(channelIndex, output = new DMatrix(widthOut, heightOut));
+            outputs.put(channelIndex, output = new DMatrix(layerWidth, layerHeight, "Output" + channelIndex));
 
-            if (!avgPool) input.maxPool(output, new int[output.getRows()][output.getCols()][2]);
-            else input.avgPool(output);
+            if (!averagePool) input.maxPool(output, new int[output.getRows()][output.getColumns()][2]);
+            else input.averagePool(output);
         }
 
         return outputs;
+    }
+
+    /**
+     * Returns layer details as string.
+     *
+     * @return layer details as string.
+     */
+    protected String getLayerDetailsByName() {
+        String layerDetailsByName = "";
+        layerDetailsByName += "Pooling type: " + (averagePool ? "Average" : "Max") + ", ";
+        layerDetailsByName += "Pool size: " + poolSize + ", ";
+        layerDetailsByName += "Stride: " + stride;
+        return layerDetailsByName;
     }
 
 }
