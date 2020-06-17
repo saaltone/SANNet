0ca5b124ca56054d1acf01c473e34466d83170c9
saaltone <simo.aaltonen@kolumbus.fi>
2020-06-17 23:14:38 +0300

    Minor updates.

--- src/core/optimization/RAdam.java
+++ src/core/optimization/RAdam.java
@@ -7,7 +7,6 @@
 package core.optimization;
 
 import utils.*;
-import utils.matrix.DMatrix;
 import utils.matrix.Matrix;
 import utils.matrix.MatrixException;
 import utils.matrix.UnaryFunctionType;
@@ -24,6 +23,12 @@ import java.util.HashMap;
 public class RAdam implements Optimizer, Serializable {
 
     private static final long serialVersionUID = -2717951798872633802L;
+
+    /**
+     * Optimization type.
+     *
+     */
+    private final OptimizationType optimizationType;
 
     /**
      * Learning rate for RAdam. Default value 0.001.
@@ -47,13 +52,13 @@ public class RAdam implements Optimizer, Serializable {
      * Hash map to store iteration counts.
      *
      */
-    private transient HashMap<Matrix, Integer> iters;
+    private transient HashMap<Matrix, Integer> iterations;
 
     /**
      * Maximum length of approximated SMA.
      *
      */
-    private double pinf;
+    private double pinf = 2 / (1 - beta2) - 1;
 
     /**
      * Hash map to store first moments (means).
@@ -70,18 +75,21 @@ public class RAdam implements Optimizer, Serializable {
     /**
      * Default constructor for RAdam.
      *
+     * @param optimizationType optimizationType.
      */
-    public RAdam() {
-        pinf = 2 / (1 - beta2) - 1;
+    public RAdam(OptimizationType optimizationType) {
+        this.optimizationType = optimizationType;
     }
 
     /**
      * Constructor for RAdam.
      *
+     * @param optimizationType optimizationType.
      * @param params parameters for RAdam.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
      */
-    public RAdam(String params) throws DynamicParamException {
+    public RAdam(OptimizationType optimizationType, String params) throws DynamicParamException {
+        this(optimizationType);
         setParams(new DynamicParam(params, getParamDefs()));
     }
 
@@ -121,7 +129,7 @@ public class RAdam implements Optimizer, Serializable {
      *
      */
     public void reset() {
-        iters = new HashMap<>();
+        iterations = new HashMap<>();
         m = new HashMap<>();
         v = new HashMap<>();
     }
@@ -129,58 +137,71 @@ public class RAdam implements Optimizer, Serializable {
     /**
      * Optimizes given weight (W) and bias (B) pair with given gradients respectively.
      *
-     * @param W weight matrix to be optimized.
-     * @param dW weight gradients for optimization step.
-     * @param B bias matrix to be optimized.
-     * @param dB bias gradients for optimization step.
+     * @param weight weight matrix to be optimized.
+     * @param weightGradient weight gradients for optimization step.
+     * @param bias bias matrix to be optimized.
+     * @param biasGradient bias gradients for optimization step.
      * @throws MatrixException throws exception if matrix operation fails.
      */
-    public void optimize(Matrix W, Matrix dW, Matrix B, Matrix dB) throws MatrixException {
-        optimize(W, dW);
-        optimize(B, dB);
+    public void optimize(Matrix weight, Matrix weightGradient, Matrix bias, Matrix biasGradient) throws MatrixException {
+        optimize(weight, weightGradient);
+        optimize(bias, biasGradient);
     }
 
     /**
      * Optimizes single matrix (M) using calculated matrix gradient (dM).<br>
      * Matrix can be for example weight or bias matrix with gradient.<br>
      *
-     * @param M matrix to be optimized.
-     * @param dM matrix gradients for optimization step.
+     * @param matrix matrix to be optimized.
+     * @param matrixGradient matrix gradients for optimization step.
      * @throws MatrixException throws exception if matrix operation fails.
      */
-    public void optimize(Matrix M, Matrix dM) throws MatrixException {
-        if (iters == null) iters = new HashMap<>();
+    public void optimize(Matrix matrix, Matrix matrixGradient) throws MatrixException {
+        if (iterations == null) iterations = new HashMap<>();
         if (m == null) m = new HashMap<>();
         if (v == null) v = new HashMap<>();
 
-        int iter;
-        if (iters.containsKey(M)) iters.put(M, iter = iters.get(M) + 1);
-        else iters.put(M, iter = 1);
+        int iteration;
+        if (iterations.containsKey(matrix)) iterations.put(matrix, iteration = iterations.get(matrix) + 1);
+        else iterations.put(matrix, iteration = 1);
 
-        Matrix mM;
-        if (m.containsKey(M)) mM = m.get(M);
-        else m.put(M, mM = new DMatrix(M.getRows(), M.getCols()));
+        Matrix vM = null;
+        if (v.containsKey(matrix)) vM = v.get(matrix);
 
-        Matrix vM;
-        if (v.containsKey(M)) vM = v.get(M);
-        else v.put(M, vM = new DMatrix(M.getRows(), M.getCols()));
+        Matrix mM = null;
+        if (m.containsKey(matrix)) mM = m.get(matrix);
 
-        mM.multiply(beta1).add(dM.multiply(1 - beta1), mM);
-        vM.multiply(beta2).add(dM.power(2).multiply(1 - beta2), vM);
+        vM = vM == null ? matrixGradient.power(2).multiply(1 - beta2) : vM.multiply(1 / beta2).add(matrixGradient.power(2).multiply(1 - beta2));
+        v.put(matrix, vM);
 
-        double beta1Iter = Math.pow(beta1, iter);
-        double beta2Iter = Math.pow(beta2, iter);
+        mM = mM == null ? matrixGradient.multiply(1 - beta1) : mM.multiply(beta1).add(matrixGradient.multiply(1 - beta1));
+        m.put(matrix, mM);
 
-        double stepSize = learningRate / (1 - beta1Iter);
+        double beta1Iteration = Math.pow(beta1, iteration);
+        double beta2Iteration = Math.pow(beta2, iteration);
 
-        double pt = pinf - 2 * iter * beta2Iter / (1 - beta2Iter);
+        Matrix mMhat = mM.multiply(1 / (1 - beta1Iteration));
+
+        double stepSize = learningRate / (1 - beta1Iteration);
+
+        double pt = pinf - 2 * iteration * beta2Iteration / (1 - beta2Iteration);
         if (pt > 4) {
-            stepSize *= Math.sqrt(((pt - 4) * (pt - 2) * pinf) / ((pinf - 4) * (pinf - 2) * pt)) * Math.sqrt(1 - beta2Iter);
-            M.subtract(mM.multiply(stepSize).divide(vM.apply(UnaryFunctionType.SQRT)), M);
+            Matrix l = vM.apply(UnaryFunctionType.MULINV).multiply(1 - beta2Iteration).apply(UnaryFunctionType.SQRT);
+            double r =  Math.sqrt(((pt - 4) * (pt - 2) * pinf) / ((pinf - 4) * (pinf - 2) * pt));
+            matrix.subtract(mMhat.multiply(l).multiply(r * stepSize), matrix);
         }
         else {
-            M.subtract(mM.multiply(stepSize), M);
+            matrix.subtract(mMhat.multiply(stepSize), matrix);
         }
+    }
+
+    /**
+     * Returns name of optimizer.
+     *
+     * @return name of optimizer.
+     */
+    public String getName() {
+        return optimizationType.toString();
     }
 
 }
