77059a036815bb749ab83e7c7905601cfbc5980c
saaltone <simo.aaltonen@kolumbus.fi>
2020-06-17 23:43:22 +0300

    Added new constructors and minor updates.

--- src/utils/matrix/DMatrix.java
+++ src/utils/matrix/DMatrix.java
@@ -20,41 +20,139 @@ public class DMatrix extends Matrix {
     private double[][] matrix;
 
     /**
-     * Constructor for dense matrix.
+     * Constructor for scalar matrix (size 1x1).
      *
-     * @param rows defines number of rows in matrix.
-     * @param cols defines number of columns in matrix.
+     * @param scalarValue value for matrix.
      */
-    public DMatrix(int rows, int cols) {
-        matrix = new double[rows][cols];
+    public DMatrix(double scalarValue) {
+        super(true);
+        matrix = new double[1][1];
+        matrix[0][0] = scalarValue;
+    }
+
+    /**
+     * Constructor for scalar matrix (size 1x1).
+     *
+     * @param scalarValue value for matrix.
+     * @param name name of matrix.
+     */
+    public DMatrix(double scalarValue, String name) {
+        super(true);
+        matrix = new double[1][1];
+        matrix[0][0] = scalarValue;
+        this.name = name;
     }
 
     /**
      * Constructor for dense matrix.
      *
      * @param rows defines number of rows in matrix.
-     * @param cols defines number of columns in matrix.
+     * @param columns defines number of columns in matrix.
+     */
+    public DMatrix(int rows, int columns) {
+        super(false);
+        matrix = new double[rows][columns];
+    }
+
+    /**
+     * Constructor for dense matrix.
+     *
+     * @param rows defines number of rows in matrix.
+     * @param columns defines number of columns in matrix.
+     * @param name name of matrix.
+     */
+    public DMatrix(int rows, int columns, String name) {
+        super(false);
+        matrix = new double[rows][columns];
+        this.name = name;
+    }
+
+    /**
+     * Constructor for dense matrix.
+     *
+     * @param rows defines number of rows in matrix.
+     * @param columns defines number of columns in matrix.
      * @param initialization type of initialization defined in class Init.
      * @param inputs applied in convolutional initialization defined as channels * filter size * filter size.
      * @param outputs applied in convolutional initialization defined as filters * filter size * filter size.
-     * @throws MatrixException throws exception if matrix operation fails.
      */
-    public DMatrix(int rows, int cols, Init initialization, int inputs, int outputs) throws MatrixException {
-        matrix = new double[rows][cols];
-        init(initialization, inputs, outputs);
+    public DMatrix(int rows, int columns, Initialization initialization, int inputs, int outputs) {
+        super(false);
+        matrix = new double[rows][columns];
+        initialize(initialization, inputs, outputs);
     }
 
     /**
      * Constructor for dense matrix.
      *
      * @param rows defines number of rows in matrix.
-     * @param cols defines number of columns in matrix.
+     * @param columns defines number of columns in matrix.
      * @param initialization type of initialization defined in class Init.
-     * @throws MatrixException throws exception if matrix operation fails.
+     * @param inputs applied in convolutional initialization defined as channels * filter size * filter size.
+     * @param outputs applied in convolutional initialization defined as filters * filter size * filter size.
+     * @param name name of matrix.
      */
-    public DMatrix(int rows, int cols, Init initialization) throws MatrixException {
-        matrix = new double[rows][cols];
-        init(initialization);
+    public DMatrix(int rows, int columns, Initialization initialization, int inputs, int outputs, String name) {
+        super(false);
+        matrix = new double[rows][columns];
+        initialize(initialization, inputs, outputs);
+        this.name = name;
+    }
+
+    /**
+     * Constructor for dense matrix.
+     *
+     * @param rows defines number of rows in matrix.
+     * @param columns defines number of columns in matrix.
+     * @param initialization type of initialization defined in class Init.
+     */
+    public DMatrix(int rows, int columns, Initialization initialization) {
+        super(false);
+        matrix = new double[rows][columns];
+        initialize(initialization);
+    }
+
+    /**
+     * Constructor for dense matrix.
+     *
+     * @param rows defines number of rows in matrix.
+     * @param columns defines number of columns in matrix.
+     * @param initialization type of initialization defined in class Init.
+     * @param name name of matrix.
+     */
+    public DMatrix(int rows, int columns, Initialization initialization, String name) {
+        super(false);
+        matrix = new double[rows][columns];
+        initialize(initialization);
+        this.name = name;
+    }
+
+    /**
+     * Constructor for dense matrix.
+     *
+     * @param rows defines number of rows in matrix.
+     * @param columns defines number of columns in matrix.
+     * @param initializer initializer.
+     * @param name name of matrix.
+     */
+    public DMatrix(int rows, int columns, Initializer initializer, String name) {
+        super(false);
+        matrix = new double[rows][columns];
+        initialize(initializer);
+        this.name = name;
+    }
+
+    /**
+     * Constructor for dense matrix.
+     *
+     * @param rows defines number of rows in matrix.
+     * @param columns defines number of columns in matrix.
+     * @param initializer initializer.
+     */
+    public DMatrix(int rows, int columns, Initializer initializer) {
+        super(false);
+        matrix = new double[rows][columns];
+        initialize(initializer);
     }
 
     /**
@@ -63,6 +161,7 @@ public class DMatrix extends Matrix {
      * @param data clones matrix data from given matrix data.
      */
     public DMatrix(double[][] data) {
+        super(false);
         matrix = data.clone();
     }
 
@@ -73,6 +172,7 @@ public class DMatrix extends Matrix {
      * @param referTo if true creates matrix with reference to given matrix data otherwise clones the data.
      */
     public DMatrix(double[][] data, boolean referTo) {
+        super(false);
         if (referTo) matrix = data;
         else matrix = data.clone();
     }
@@ -98,22 +198,22 @@ public class DMatrix extends Matrix {
      * Matrix function used to set value of specific row and column.
      *
      * @param row row of value to be set.
-     * @param col column of value to be set.
+     * @param column column of value to be set.
      * @param value new value to be set.
      */
-    public void setValue(int row, int col, double value) {
-        matrix[!t ? row : col][!t ? col : row] = value;
+    public void setValue(int row, int column, double value) {
+        matrix[!isTransposed ? row : column][!isTransposed ? column : row] = value;
     }
 
     /**
      * Matrix function used to get value of specific row and column.
      *
      * @param row row of value to be returned.
-     * @param col column of value to be returned.
+     * @param column column of value to be returned.
      * @return value of row and column.
      */
-    public double getValue(int row, int col) {
-        return matrix[!t ? row : col][!t ? col : row];
+    public double getValue(int row, int column) {
+        return matrix[isScalar() ? 0 : !isTransposed ? row : column][isScalar() ? 0 : !isTransposed ? column : row];
     }
 
     /**
@@ -121,7 +221,7 @@ public class DMatrix extends Matrix {
      *
      * @return size of matrix.
      */
-    public int getSize() {
+    public int size() {
         return matrix.length * matrix[0].length;
     }
 
@@ -131,7 +231,7 @@ public class DMatrix extends Matrix {
      * @return number of rows in matrix.
      */
     public int getRows() {
-        return !t ? matrix.length : matrix[0].length;
+        return !isTransposed ? matrix.length : matrix[0].length;
     }
 
     /**
@@ -139,19 +239,17 @@ public class DMatrix extends Matrix {
      *
      * @return number of columns in matrix.
      */
-    public int getCols() {
-        return !t ? matrix[0].length : matrix.length;
+    public int getColumns() {
+        return !isTransposed ? matrix[0].length : matrix.length;
     }
 
     /**
      * Returns new matrix of dimensions rows x columns
      *
-     * @param rows amount of rows for new matrix.
-     * @param cols amount of columns for new matrix.
      * @return new matrix of dimensions rows x columns.
      */
-    protected Matrix getNewMatrix(int rows, int cols) {
-        return new DMatrix(rows, cols);
+    public Matrix getNewMatrix() {
+        return isScalar() ? new DMatrix(0) : new DMatrix(getRows(), getColumns());
     }
 
     /**
@@ -159,11 +257,11 @@ public class DMatrix extends Matrix {
      *
      * @param newMatrix new matrix to be copied inside this matrix.
      */
-    protected void setAsMatrix(Matrix newMatrix) {
-        matrix = new double[newMatrix.getRows()][newMatrix.getCols()];
+    protected void copyMatrixData(Matrix newMatrix) {
+        matrix = new double[newMatrix.getRows()][newMatrix.getColumns()];
         for (int row = 0; row < newMatrix.getRows(); row++) {
-            for (int col = 0; col < newMatrix.getCols(); col++) {
-                setValue(row, col, newMatrix.getValue(row, col));
+            for (int column = 0; column < newMatrix.getColumns(); column++) {
+                setValue(row, column, newMatrix.getValue(row, column));
             }
         }
     }
