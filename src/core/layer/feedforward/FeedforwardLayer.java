b1892a872573f0ddb462b3fe30672a2192570faf
saaltone <simo.aaltonen@kolumbus.fi>
2020-06-17 23:56:22 +0300

    Minor updates.

--- src/core/layer/feedforward/FeedforwardLayer.java
+++ src/core/layer/feedforward/FeedforwardLayer.java
@@ -9,16 +9,10 @@ package core.layer.feedforward;
 import core.NeuralNetworkException;
 import core.activation.ActivationFunction;
 import core.layer.AbstractExecutionLayer;
-import core.layer.AbstractLayer;
-import core.normalization.Normalization;
 import utils.*;
-import utils.matrix.DMatrix;
-import utils.matrix.Init;
-import utils.matrix.Matrix;
-import utils.matrix.MatrixException;
+import utils.matrix.*;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  * Implements non-recurrent feed forward layer.
@@ -30,19 +24,31 @@ public class FeedforwardLayer extends AbstractExecutionLayer {
      * Weight matrix.
      *
      */
-    private Matrix W;
+    private Matrix weight;
 
     /**
      * Bias matrix.
      *
      */
-    private Matrix B;
+    private Matrix bias;
+
+    /**
+     * Activation function for feedforward layer.
+     *
+     */
+    protected final ActivationFunction activationFunction;
 
     /**
      * True if weights are regulated otherwise weights are not regulated.
      *
      */
     private boolean regulateDirectWeights = true;
+
+    /**
+     * If true normalizes at input otherwise normalizes prior non-linearity.
+     *
+     */
+    private boolean normalizeAtInput = false;
 
     /**
      * Input matrix for procedure construction.
@@ -53,15 +59,19 @@ public class FeedforwardLayer extends AbstractExecutionLayer {
     /**
      * Constructor for feed forward layer.
      *
-     * @param parent reference to parent layer.
-     * @param activation activation function used.
-     * @param initialization intialization function for weight.
+     * @param layerIndex layer Index.
+     * @param activationFunction activation function used.
+     * @param initialization initialization function for weight.
      * @param params parameters for feed forward layer.
      * @throws NeuralNetworkException throws exception if setting of activation function fails.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws MatrixException throws exception if custom function is attempted to be created with this constructor.
      */
-    public FeedforwardLayer(AbstractLayer parent, ActivationFunction activation, Init initialization, String params) throws NeuralNetworkException, DynamicParamException {
-        super (parent, activation, initialization, params);
+    public FeedforwardLayer(int layerIndex, ActivationFunction activationFunction, Initialization initialization, String params) throws NeuralNetworkException, DynamicParamException, MatrixException {
+        super (layerIndex, initialization, params);
+        setParams(new DynamicParam(params, getParamDefs()));
+        if (activationFunction != null) this.activationFunction = activationFunction;
+        else this.activationFunction = new ActivationFunction(UnaryFunctionType.ELU);
     }
 
     /**
@@ -70,9 +80,9 @@ public class FeedforwardLayer extends AbstractExecutionLayer {
      * @return parameters used for feed forward layer.
      */
     public HashMap<String, DynamicParam.ParamType> getParamDefs() {
-        HashMap<String, DynamicParam.ParamType> paramDefs = new HashMap<>();
-        paramDefs.put("width", DynamicParam.ParamType.INT);
+        HashMap<String, DynamicParam.ParamType> paramDefs = new HashMap<>(super.getParamDefs());
         paramDefs.put("regulateDirectWeights", DynamicParam.ParamType.BOOLEAN);
+        paramDefs.put("normalizeAtInput", DynamicParam.ParamType.BOOLEAN);
         return paramDefs;
     }
 
@@ -80,15 +90,17 @@ public class FeedforwardLayer extends AbstractExecutionLayer {
      * Sets parameters used for feed forward layer.<br>
      * <br>
      * Supported parameters are:<br>
-     *     - width: width (number of nodes) of feed forward layer.<br>
      *     - regulateDirectWeights: true if (direct) weights are regulated otherwise false (default value).<br>
+     *     - normalizeAtInput: if true normalizes at input otherwise normalizes prior non-linearity.
      *
      * @param params parameters used for feed forward layer.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws NeuralNetworkException throws exception if minimum layer dimensions are not met.
      */
-    public void setParams(DynamicParam params) throws DynamicParamException {
-        if (params.hasParam("width")) parent.setWidth(params.getValueAsInteger("width"));
+    public void setParams(DynamicParam params) throws DynamicParamException, NeuralNetworkException {
+        super.setParams(params);
         if (params.hasParam("regulateDirectWeights")) regulateDirectWeights = params.getValueAsBoolean("regulateDirectWeights");
+        if (params.hasParam("normalizeAtInput")) normalizeAtInput = params.getValueAsBoolean("normalizeAtInput");
     }
 
     /**
@@ -109,63 +121,64 @@ public class FeedforwardLayer extends AbstractExecutionLayer {
      * Initializes feed forward layer.<br>
      * Initializes weight and bias and their gradients.<br>
      *
-     * @throws MatrixException throws exception if matrix operation fails.
      */
-    public void initialize() throws MatrixException {
-        int pLayerWidth = parent.getBackward().getPLayerWidth();
-        int nLayerWidth = parent.getBackward().getNLayerWidth();
+    public void initialize() {
+        int previousLayerWidth = getPreviousLayerWidth();
+        int layerWidth = getLayerWidth();
 
-        W = new DMatrix(nLayerWidth, pLayerWidth, this.initialization);
+        weight = new DMatrix(layerWidth, previousLayerWidth, this.initialization, "Weight");
 
-        B = new DMatrix(nLayerWidth, 1);
+        bias = new DMatrix(layerWidth, 1, "Bias");
 
-        parent.getBackward().registerWeight(W, true, regulateDirectWeights, true);
+        registerWeight(weight, regulateDirectWeights, true);
 
-        parent.getBackward().registerWeight(B, true, false, false);
+        registerWeight(bias, false, false);
 
-    }
-
-    /**
-     * Resets input.
-     *
-     * @param resetPreviousInput if true resets also previous input.
-     * @throws MatrixException throws exception if matrix operation fails.
-     */
-    protected void resetInput(boolean resetPreviousInput) throws MatrixException {
-        input = new DMatrix(parent.getBackward().getPLayerWidth(), 1, Init.ONE);
     }
 
     /**
      * Returns input matrices for procedure construction.
      *
-     * @return input matrices for procedure construction.
+     * @param resetPreviousInput if true resets also previous input.
+     * @return input matrix for procedure construction.
      */
-    protected Sample getInputMatrices() throws MatrixException {
-        Sample inputs = new Sample(1);
-        inputs.put(0, input);
-        return inputs;
+    public MMatrix getInputMatrices(boolean resetPreviousInput) {
+        input = new DMatrix(getPreviousLayerWidth(), 1, Initialization.ONE, "Input");
+        return new MMatrix(input);
     }
 
     /**
      * Builds forward procedure and implicitly builds backward procedure.
      *
-     * @param normalizers normalizers for layer normalization.
      * @return output of forward procedure.
      * @throws MatrixException throws exception if matrix operation fails.
      */
-    protected Sample getForwardProcedure(HashSet<Normalization> normalizers) throws MatrixException {
-        Matrix output = W.dot(input);
+    public MMatrix getForwardProcedure() throws MatrixException {
+        if (normalizeAtInput) input.setNormalize(true);
+        input.setRegularize(true);
 
-        if (normalizers.size() > 0) output.setNormalization(normalizers);
+        Matrix output = weight.dot(input);
 
-        output = output.add(B);
+        if (!normalizeAtInput) output.setNormalize(true);
 
-        output = output.apply(activation);
+        output = output.add(bias);
 
-        Sample outputs = new Sample(1);
+        output = output.apply(activationFunction);
+
+        output.setName("Output");
+        MMatrix outputs = new MMatrix(1, "Output");
         outputs.put(0, output);
         return outputs;
 
+    }
+
+    /**
+     * Returns layer details as string.
+     *
+     * @return layer details as string.
+     */
+    protected String getLayerDetailsByName() {
+        return "Activation function: " + activationFunction.getName();
     }
 
 }
