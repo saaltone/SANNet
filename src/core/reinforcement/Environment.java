1b0a55946694b1685e788511ba6f148ff16d94fc
saaltone <simo.aaltonen@kolumbus.fi>
2020-09-09 00:29:58 +0300

    Refactored Environment interface.

--- src/core/reinforcement/Environment.java
+++ src/core/reinforcement/Environment.java
@@ -5,49 +5,32 @@
 
 package core.reinforcement;
 
-import core.NeuralNetworkException;
-import utils.DynamicParamException;
-import utils.matrix.Matrix;
-import utils.matrix.MatrixException;
-
-import java.util.HashSet;
-
 /**
- * Interface for environment.
+ * Interface defining Environment.
  *
  */
 public interface Environment {
+
+    /**
+     * Returns true if environment is episodic otherwise false.
+     *
+     * @return true if environment is episodic otherwise false.
+     */
+    boolean isEpisodic();
 
     /**
      * Returns current state of environment.
      *
      * @return state of environment.
      */
-    Matrix getState();
+    EnvironmentState getState();
 
     /**
-     * True if state is terminal. This is usually true if episode is completed.
-     *
-     * @return true if state is terminal.
-     */
-    boolean isTerminalState();
-
-    /**
-     * Returns available actions in current state of environment
-     *
-     * @return available actions in current state of environment.
-     */
-    HashSet<Integer> getAvailableActions();
-
-    /**
-     * Takes specific action.
+     * Takes specific action in environment.
      *
      * @param agent agent that is taking action.
      * @param action action to be taken.
-     * @throws NeuralNetworkException throws exception if neural network operation fails.
-     * @throws MatrixException throws exception if matrix operation fails.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
      */
-    void commitAction(Agent agent, int action) throws NeuralNetworkException, MatrixException, DynamicParamException;
+    void commitAction(Agent agent, int action);
 
 }
