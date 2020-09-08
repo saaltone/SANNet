8ad1951bb7eb703638b390c38ca55557ec40ff64
saaltone <simo.aaltonen@kolumbus.fi>
2020-09-09 00:51:17 +0300

    Added dedicated ActionValueFunctionEstimator.

---
+++ src/core/reinforcement/value/ActionValueFunctionEstimator.java
@@ -0,0 +1,48 @@
+/*
+ * SANNet Neural Network Framework
+ * Copyright (C) 2018 - 2020 Simo Aaltonen
+ */
+
+package core.reinforcement.value;
+
+import core.reinforcement.function.FunctionEstimator;
+import core.reinforcement.memory.StateTransition;
+import utils.DynamicParamException;
+
+/**
+ * Class that defines ActionValueFunctionEstimator (action value function with function estimator).
+ *
+ */
+public class ActionValueFunctionEstimator extends AbstractValueFunctionEstimator {
+
+    /**
+     * Constructor for ActionValueFunctionEstimator
+     *
+     * @param functionEstimator reference to FunctionEstimator.
+     */
+    public ActionValueFunctionEstimator(FunctionEstimator functionEstimator) {
+        super(functionEstimator.getNumberOfActions(), functionEstimator);
+    }
+
+    /**
+     * Constructor for ActionValueFunctionEstimator
+     *
+     * @param functionEstimator reference to FunctionEstimator.
+     * @param params parameters for value function.
+     * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     */
+    public ActionValueFunctionEstimator(FunctionEstimator functionEstimator, String params) throws DynamicParamException {
+        super(functionEstimator.getNumberOfActions(), functionEstimator, params);
+    }
+
+    /**
+     * Returns target value based on next state.
+     *
+     * @param nextStateTransition next state transition.
+     * @return target value based on next state
+     */
+    public double getTargetValue(StateTransition nextStateTransition) {
+        return nextStateTransition.tdTarget;
+    }
+
+}
