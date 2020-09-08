5a7c948e7d7d30d49298ad5e9d2e4ff1399ceaf4
saaltone <simo.aaltonen@kolumbus.fi>
2020-09-09 00:29:01 +0300

    Refactored agent interface.

--- src/core/reinforcement/Agent.java
+++ src/core/reinforcement/Agent.java
@@ -10,7 +10,7 @@ import utils.DynamicParamException;
 import utils.matrix.MatrixException;
 
 /**
- * Interface for agent.
+ * Interface defining agent.
  *
  */
 public interface Agent {
@@ -19,7 +19,7 @@ public interface Agent {
      * Starts agent.
      *
      * @throws NeuralNetworkException throws exception if start of neural network estimator(s) fails.
-     * @throws MatrixException throws exception if depth of matrix is less than 1.
+     * @throws MatrixException throws exception if matrix operation fails.
      */
     void start() throws NeuralNetworkException, MatrixException;
 
@@ -30,16 +30,30 @@ public interface Agent {
     void stop();
 
     /**
-     * Starts new episode. Resets sequence by default.
+     * Starts new episode for episodic environments.
      *
      */
     void newEpisode();
 
     /**
-     * Begins new episode step for agent.
+     * Begins new (episode) step for agent.
      *
+     * @throws NeuralNetworkException throws exception if neural network operation fails.
+     * @throws MatrixException throws exception if matrix operation fails.
+     * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws AgentException throws exception if memory instances of value and policy function are not equal.
      */
-    void newStep();
+    void newStep() throws MatrixException, DynamicParamException, NeuralNetworkException, AgentException;
+
+    /**
+     * Ends episode.
+     *
+     * @throws NeuralNetworkException throws exception if neural network operation fails.
+     * @throws MatrixException throws exception if matrix operation fails.
+     * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws AgentException throws exception if update of estimator fails.
+     */
+    void endEpisode() throws MatrixException, NeuralNetworkException, DynamicParamException, AgentException;
 
     /**
      * Disables learning.
@@ -54,33 +68,27 @@ public interface Agent {
     void enableLearning();
 
     /**
-     * Takes action as defined by agent's policy.
+     * Takes action per defined agent policy.
      *
      * @throws NeuralNetworkException throws exception if neural network operation fails.
      * @throws MatrixException throws exception if matrix operation fails.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
      */
-    void act() throws NeuralNetworkException, MatrixException, DynamicParamException;
+    void act() throws NeuralNetworkException, MatrixException;
 
     /**
-     * Takes action per given policy.
+     * Takes action per defined agent policy.
      *
      * @param alwaysGreedy if true greedy action is always taken.
      * @throws NeuralNetworkException throws exception if neural network operation fails.
      * @throws MatrixException throws exception if matrix operation fails.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
      */
-    void act(boolean alwaysGreedy) throws NeuralNetworkException, MatrixException, DynamicParamException;
+    void act(boolean alwaysGreedy) throws NeuralNetworkException, MatrixException;
 
     /**
-     * Response from environment after agent commits action.
+     * Assigns immediate reward from environment in response to action agent executed.
      *
-     * @param reward immediate reward.
-     * @param finalState if true state is final otherwise false.
-     * @throws NeuralNetworkException throws exception if neural network operation fails.
-     * @throws MatrixException throws exception if matrix operation fails.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     *  @param reward immediate reward.
      */
-    void respond(double reward, boolean finalState) throws NeuralNetworkException, MatrixException, DynamicParamException;
+    void respond(double reward);
 
 }
