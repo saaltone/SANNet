9211a3e64f43372e053f9d162099edf172af43bc
saaltone <simo.aaltonen@kolumbus.fi>
2020-10-28 01:40:04 +0200

    Added reset, procedure recording, finish and update function. Added support for executing action by external agent.

--- src/core/reinforcement/policy/executablepolicy/ExecutablePolicy.java
+++ src/core/reinforcement/policy/executablepolicy/ExecutablePolicy.java
@@ -5,6 +5,7 @@
 
 package core.reinforcement.policy.executablepolicy;
 
+import core.reinforcement.memory.StateTransition;
 import utils.matrix.Matrix;
 
 import java.util.HashSet;
@@ -16,10 +17,27 @@ import java.util.HashSet;
 public interface ExecutablePolicy {
 
     /**
+     * Resets policy.
+     *
+     * @param forceReset forces to trigger reset.
+     */
+    void reset(boolean forceReset);
+
+    /**
      * Increments policy.
      *
      */
     void increment();
+
+    /**
+     * Takes action decided by external agent.
+     *
+     * @param stateValueMatrix current state value matrix.
+     * @param availableActions available actions in current state
+     * @param stateValueOffset state value offset
+     * @param action action.
+     */
+    void action(Matrix stateValueMatrix, HashSet<Integer> availableActions, int stateValueOffset, int action);
 
     /**
      * Takes action based on policy.
@@ -27,8 +45,27 @@ public interface ExecutablePolicy {
      * @param stateValueMatrix current state value matrix.
      * @param availableActions available actions in current state
      * @param stateValueOffset state value offset
+     * @param alwaysGreedy if true greedy action is always taken.
      * @return action taken.
      */
-    int action(Matrix stateValueMatrix, HashSet<Integer> availableActions, int stateValueOffset);
+    int action(Matrix stateValueMatrix, HashSet<Integer> availableActions, int stateValueOffset, boolean alwaysGreedy);
+
+    /**
+     * Records state transition for action execution.
+     *
+     * @param stateTransition state transition.
+     */
+    void record(StateTransition stateTransition);
+
+    /**
+     * Updates policy.
+     *
+     */
+    void update();
+    /**
+     * Finishes episode.
+     *
+     */
+    void finish();
 
 }
