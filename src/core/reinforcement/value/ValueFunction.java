f8f7ed1e4eebd775fbebfc41b6ac29ae0449697b
saaltone <simo.aaltonen@kolumbus.fi>
2020-10-28 01:41:15 +0200

    Added support for update function. Minor updates.

--- src/core/reinforcement/value/ValueFunction.java
+++ src/core/reinforcement/value/ValueFunction.java
@@ -9,6 +9,7 @@ import core.NeuralNetworkException;
 import core.reinforcement.Agent;
 import core.reinforcement.AgentException;
 import core.reinforcement.function.FunctionEstimator;
+import core.reinforcement.memory.StateTransition;
 import utils.DynamicParamException;
 import utils.matrix.MatrixException;
 
@@ -23,8 +24,9 @@ public interface ValueFunction {
      *
      * @throws NeuralNetworkException throws exception if starting of value function fails.
      * @throws MatrixException throws exception if depth of matrix is less than 1.
+     * @throws DynamicParamException throws exception if parameter (params) setting fails.
      */
-    void start() throws NeuralNetworkException, MatrixException;
+    void start() throws NeuralNetworkException, MatrixException, DynamicParamException;
 
     /**
      * Stops FunctionEstimator
@@ -35,13 +37,30 @@ public interface ValueFunction {
     /**
      * Updates value function.
      *
+     * @throws NeuralNetworkException throws exception if neural network operation fails.
+     * @throws MatrixException throws exception if matrix operation fails.
+     */
+    void update() throws MatrixException, NeuralNetworkException;
+
+    /**
+     * Updates values for current episode.
+     *
+     * @param stateTransition state transition.
+     * @throws MatrixException throws exception if matrix operation fails.
+     * @throws NeuralNetworkException throws exception if neural network operation fails.
+     */
+    void update(StateTransition stateTransition) throws NeuralNetworkException, MatrixException;
+
+    /**
+     * Updates FunctionEstimator.
+     *
      * @param agent agent.
      * @throws NeuralNetworkException throws exception if neural network operation fails.
      * @throws MatrixException throws exception if matrix operation fails.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
      * @throws AgentException throws exception if function estimator update fails.
      */
-    void update(Agent agent) throws MatrixException, NeuralNetworkException, DynamicParamException, AgentException;
+    void updateFunctionEstimator(Agent agent) throws NeuralNetworkException, MatrixException, DynamicParamException, AgentException;
 
     /**
      * Returns FunctionEstimator.
