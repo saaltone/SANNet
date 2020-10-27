d3e83469dc8da47067470492be8a8b63cd61f8fa
saaltone <simo.aaltonen@kolumbus.fi>
2020-10-28 01:53:38 +0200

    Replaces interface with class functionality.

--- src/core/reinforcement/policy/ActionablePolicy.java
+++ src/core/reinforcement/policy/ActionablePolicy.java
@@ -5,94 +5,39 @@
 
 package core.reinforcement.policy;
 
-import core.NeuralNetworkException;
 import core.reinforcement.Agent;
-import core.reinforcement.AgentException;
-import core.reinforcement.Environment;
-import core.reinforcement.memory.StateTransition;
 import core.reinforcement.function.FunctionEstimator;
-import utils.DynamicParamException;
-import utils.matrix.MatrixException;
+import core.reinforcement.policy.executablepolicy.ExecutablePolicy;
 
 /**
- * Interface for ActionablePolicy.
+ * Class that defines ActionablePolicy.
  *
  */
-public interface ActionablePolicy {
+public class ActionablePolicy extends AbstractPolicy {
 
     /**
-     * Starts ActionablePolicy.
+     * Constructor for ActionablePolicy.
      *
-     * @throws NeuralNetworkException throws exception if start of neural network estimator(s) fails.
-     * @throws MatrixException throws exception if depth of matrix is less than 1.
+     * @param executablePolicy reference to executable policy.
+     * @param functionEstimator reference to FunctionEstimator.
      */
-    void start() throws NeuralNetworkException, MatrixException;
-
-    /**
-     * Stops ActionablePolicy.
-     *
-     */
-    void stop();
-
-    /**
-     * Sets reference to environment.
-     *
-     * @param environment reference to environment.
-     */
-    void setEnvironment(Environment environment);
-
-    /**
-     * Returns reference to environment.
-     *
-     * @return reference to environment.
-     */
-    Environment getEnvironment();
-
-    /**
-     * Set flag if agent is in learning mode.
-     *
-     * @param isLearning if true agent is in learning mode.
-     */
-    void setLearning(boolean isLearning);
-
-    /**
-     * Return flag is policy is in learning mode.
-     *
-     * @return if true agent is in learning mode.
-     */
-    boolean isLearning();
+    public ActionablePolicy(ExecutablePolicy executablePolicy, FunctionEstimator functionEstimator) {
+        super(executablePolicy, functionEstimator);
+    }
 
     /**
      * Updates policy.
      *
      */
-    void update();
-
-    /**
-     * Takes action by applying defined executable policy,
-     *
-     * @param stateTransition state transition.
-     * @throws NeuralNetworkException throws exception if neural network operation fails.
-     * @throws MatrixException throws exception if matrix operation fails.
-     */
-    void act(StateTransition stateTransition) throws NeuralNetworkException, MatrixException;
+    public void update() {
+    }
 
     /**
      * Updates policy.
      *
      * @param agent agent.
-     * @throws NeuralNetworkException throws exception if neural network operation fails.
-     * @throws MatrixException throws exception if matrix operation fails.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     * @throws AgentException throws exception if function estimator update fails.
      */
-    void update(Agent agent) throws NeuralNetworkException, MatrixException, DynamicParamException, AgentException;
-
-    /**
-     * Returns FunctionEstimator.
-     *
-     * @return FunctionEstimator.
-     */
-    FunctionEstimator getFunctionEstimator();
+    public void update(Agent agent) {
+    }
 
 }
