99bc74f986995160dcec8ac8219964a6a58d05ab
saaltone <simo.aaltonen@kolumbus.fi>
2020-11-01 22:20:33 +0200

    Minor update.

--- src/utils/procedure/NormExpression.java
+++ src/utils/procedure/NormExpression.java
@@ -15,7 +15,7 @@ public class NormExpression extends AbstractUnaryExpression implements Serializa
      * Name of operation.
      *
      */
-    private static final String operationName = "NORM";
+    private static final String expressionName = "NORM";
 
     /**
      * Power of norm.
@@ -33,7 +33,7 @@ public class NormExpression extends AbstractUnaryExpression implements Serializa
      * @throws MatrixException throws exception if expression arguments are not defined or norm p value is not at least 2.
      */
     public NormExpression(int expressionID, Node argument1, Node result, int p) throws MatrixException {
-        super(operationName, operationName, expressionID, argument1, result);
+        super(expressionName, expressionName, expressionID, argument1, result);
         if (p < 2) throw new MatrixException("Norm p value must be at least 2.");
         this.p = p;
     }
@@ -52,7 +52,7 @@ public class NormExpression extends AbstractUnaryExpression implements Serializa
      * @throws MatrixException throws exception if calculation fails.
      */
     public void calculateExpression(int index) throws MatrixException {
-        if (argument1.getMatrix(index) == null) throw new MatrixException("Arguments for NORM operation not defined");
+        if (argument1.getMatrix(index) == null) throw new MatrixException(expressionName + "Arguments for operation not defined");
         result.setMatrix(index, argument1.getMatrix(index).normAsMatrix(p));
     }
 
@@ -70,7 +70,7 @@ public class NormExpression extends AbstractUnaryExpression implements Serializa
      * @throws MatrixException throws exception if calculation of gradient fails.
      */
     public void calculateGradient(int index) throws MatrixException {
-        if (result.getGradient(index) == null) throw new MatrixException("Result gradient not defined.");
+        if (result.getGradient(index) == null) throw new MatrixException(expressionName + ": Result gradient not defined.");
         // https://math.stackexchange.com/questions/1482494/derivative-of-the-l-p-norm/1482525
         argument1.updateGradient(index, result.getGradient(index).multiply(argument1.getMatrix(index).applyBi(result.getMatrix(index), (value, constant) -> Math.pow(Math.abs(value) / constant, p - 1) * Math.signum(value))), true);
     }
