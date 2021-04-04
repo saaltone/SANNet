53166f963cbfa24040449e56c8e18225f42fbdaf
saaltone <simo.aaltonen@kolumbus.fi>
2021-04-05 01:24:40 +0300

    Refactored to align with new mask interface and abstract mask class. Removed mask stacking functionality.

--- src/utils/matrix/SMask.java
+++ src/utils/matrix/SMask.java
@@ -6,13 +6,12 @@
 package utils.matrix;
 
 import java.util.HashMap;
-import java.util.Stack;
 
 /**
  * Implements sparse mask to mask sparse matrices.
  *
  */
-public class SMask extends Mask {
+public class SMask extends AbstractMask {
 
     /**
      * Defines number of rows in mask.
@@ -45,24 +44,6 @@ public class SMask extends Mask {
     private HashMap<Integer, Boolean> columnMask = new HashMap<>();
 
     /**
-     * Stack to store masks.
-     *
-     */
-    private Stack<HashMap<Integer, Boolean>> maskStack = new Stack<>();
-
-    /**
-     * Stack to store row masks.
-     *
-     */
-    private Stack<HashMap<Integer, Boolean>> rowMaskStack = new Stack<>();
-
-    /**
-     * Stack to store column masks.
-     *
-     */
-    private Stack<HashMap<Integer, Boolean>> columnMaskStack = new Stack<>();
-
-    /**
      * Constructor for sparse mask.
      *
      * @param rows defines number of rows in mask.
@@ -81,17 +62,34 @@ public class SMask extends Mask {
      * @param data mask data.
      * @param rowData row mask data.
      * @param columnData column mask data.
-     * @param transposed if true mask if transposed otherwise false.
      * @param probability probability of masking.
      */
-    protected SMask(int rows, int columns, HashMap<Integer, Boolean> data, HashMap<Integer, Boolean> rowData, HashMap<Integer, Boolean> columnData, boolean transposed, double probability) {
+    protected SMask(int rows, int columns, HashMap<Integer, Boolean> data, HashMap<Integer, Boolean> rowData, HashMap<Integer, Boolean> columnData, double probability) {
         this.rows = rows;
         this.columns = columns;
         mask.putAll(data);
         rowMask.putAll(rowData);
         columnMask.putAll(columnData);
-        this.isTransposed = transposed;
         this.probability = probability;
+    }
+
+    /**
+     * Returns new mask of dimensions rows x columns.<br>
+     *
+     * @return new mask of dimensions rows x columns.
+     */
+    public Mask getNewMask() {
+        return new SMask(getRows(), getColumns());
+    }
+
+    /**
+     * Returns new mask of dimensions rows x columns.<br>
+     *
+     * @param asTransposed if true returns new mask as transposed otherwise with unchanged dimensions.
+     * @return new mask of dimensions rows x columns.
+     */
+    public Mask getNewMask(boolean asTransposed) {
+        return !asTransposed ? new SMask(getRows(), getColumns()) : new SMask(getColumns(), getRows());
     }
 
     /**
@@ -100,7 +98,7 @@ public class SMask extends Mask {
      * @return copy of mask.
      */
     public Mask getCopy() {
-        return new SMask(rows, columns, mask, rowMask, columnMask, isTransposed, probability);
+        return new SMask(rows, columns, mask, rowMask, columnMask, probability);
     }
 
     /**
@@ -118,7 +116,7 @@ public class SMask extends Mask {
      * @return number of rows in mask.
      */
     public int getRows() {
-        return !isTransposed ? rows : columns;
+        return rows;
     }
 
     /**
@@ -127,7 +125,7 @@ public class SMask extends Mask {
      * @return number of columns in mask.
      */
     public int getColumns() {
-        return !isTransposed ? columns : rows;
+        return columns;
     }
 
     /**
@@ -138,9 +136,7 @@ public class SMask extends Mask {
      * @param value defines if specific row and column is masked (true) or not (false).
      */
     public void setMask(int row, int column, boolean value) {
-        int currentRow = !isTransposed ? row : column;
-        int currentColumn = !isTransposed ? column : row;
-        mask.put(currentRow * columns + currentColumn, value);
+        mask.put(row * columns + column, value);
     }
 
     /**
@@ -152,9 +148,7 @@ public class SMask extends Mask {
      */
     public boolean getMask(int row, int column) {
         if (mask == null) return false;
-        int currentRow = !isTransposed ? row : column;
-        int currentColumn = !isTransposed ? column : row;
-        return mask.getOrDefault(currentRow * columns + currentColumn, false);
+        return mask.getOrDefault(row * columns + column, false);
     }
 
     /**
@@ -165,85 +159,6 @@ public class SMask extends Mask {
         mask = new HashMap<>();
         rowMask = new HashMap<>();
         columnMask = new HashMap<>();
-        maskStack = new Stack<>();
-        rowMaskStack = new Stack<>();
-        columnMaskStack = new Stack<>();
-    }
-
-    /**
-     * Pushes current mask into stack and optionally creates new mask.<br>
-     * Useful in operations where sequence of operations and taken between matrices.<br>
-     *
-     * @param reset if true new mask is generated after current mask is stacked.
-     */
-    public void stackMask(boolean reset) {
-        maskStack.push(mask);
-        if (reset) mask = new HashMap<>();
-    }
-
-    /**
-     * Pops mask from mask stack.
-     *
-     * @throws MatrixException throws exception if mask stack is empty.
-     */
-    public void unstackMask() throws MatrixException {
-        if (maskStack.isEmpty()) throw new MatrixException("Mask stack is empty.");
-        mask = maskStack.pop();
-    }
-
-    /**
-     * Returns size of a mask stack.
-     *
-     * @return Size of mask stack.
-     */
-    public int maskStackSize() {
-        return maskStack.size();
-    }
-
-    /**
-     * Clears mask stack.
-     *
-     */
-    public void clearMaskStack() {
-        maskStack = new Stack<>();
-    }
-
-    /**
-     * Pushes current row mask into stack and optionally creates new mask.<br>
-     * Useful in operations where sequence of operations and taken between matrices.<br>
-     *
-     * @param reset if true new mask is generated after current mask is stacked.
-     */
-    public void stackRowMask(boolean reset) {
-        rowMaskStack.push(rowMask);
-        if (reset) rowMask = new HashMap<>();
-    }
-
-    /**
-     * Pops row mask from mask stack.
-     *
-     * @throws MatrixException throws exception if row mask stack is empty.
-     */
-    public void unstackRowMask() throws MatrixException {
-        if (rowMaskStack.isEmpty()) throw new MatrixException("Row mask stack is empty.");
-        rowMask = rowMaskStack.pop();
-    }
-
-    /**
-     * Returns size of a row mask stack.
-     *
-     * @return size of row mask stack.
-     */
-    public int rowMaskStackSize() {
-        return rowMaskStack.size();
-    }
-
-    /**
-     * Clears row mask stack.
-     *
-     */
-    public void clearRowMaskStack() {
-        rowMaskStack = new Stack<>();
     }
 
     /**
@@ -253,8 +168,7 @@ public class SMask extends Mask {
      * @param value if true sets row mask otherwise unsets mask.
      */
     public void setRowMask(int row, boolean value) {
-        if (!isTransposed) rowMask.put(row, value);
-        else columnMask.put(row, value);
+        rowMask.put(row, value);
     }
 
     /**
@@ -265,7 +179,7 @@ public class SMask extends Mask {
      */
     public boolean getRowMask(int row) {
         if (mask == null) return false;
-        return !isTransposed ? rowMask.getOrDefault(row, false) : columnMask.getOrDefault(row, false);
+        return rowMask.getOrDefault(row, false);
     }
 
     /**
@@ -275,8 +189,7 @@ public class SMask extends Mask {
      * @param value if true sets row mask otherwise unsets mask.
      */
     public void setColumnMask(int column, boolean value) {
-        if (!isTransposed) columnMask.put(column, value);
-        else rowMask.put(column, value);
+        columnMask.put(column, value);
     }
 
     /**
@@ -287,45 +200,7 @@ public class SMask extends Mask {
      */
     public boolean getColumnMask(int column) {
         if (mask == null) return false;
-        return !isTransposed ? columnMask.getOrDefault(column, false) : rowMask.getOrDefault(column, false);
-    }
-
-    /**
-     * Pushes current column mask into stack and optionally creates new mask.<br>
-     * Useful in operations where sequence of operations and taken between matrices.<br>
-     *
-     * @param reset if true new mask is generated after current mask is stacked.
-     */
-    public void stackColumnMask(boolean reset) {
-        columnMaskStack.push(columnMask);
-        if (reset) columnMask = new HashMap<>();
-    }
-
-    /**
-     * Pops column mask from mask stack.
-     *
-     * @throws MatrixException throws exception if column mask stack is empty.
-     */
-    public void unstackColumnMask() throws MatrixException {
-        if (columnMaskStack.isEmpty()) throw new MatrixException("Column mask stack is empty.");
-        columnMask = columnMaskStack.pop();
-    }
-
-    /**
-     * Returns size of a column mask stack.
-     *
-     * @return size of column mask stack.
-     */
-    public int columnMaskStackSize() {
-        return columnMaskStack.size();
-    }
-
-    /**
-     * Clears column mask stack.
-     *
-     */
-    public void clearColumnMaskStack() {
-        columnMaskStack = new Stack<>();
+        return columnMask.getOrDefault(column, false);
     }
 
 }
