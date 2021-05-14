c25a28dca0bae38af134d9c024205057d8256049
saaltone <simo.aaltonen@kolumbus.fi>
2021-05-14 23:05:17 +0300

    Cosmetic updates.

--- src/core/layer/LayerType.java
+++ src/core/layer/LayerType.java
@@ -6,30 +6,63 @@
 package core.layer;
 
 /**
- * Defines supported neural network layer types.<br>
- * <br>
- * Currently supports types are:
- *     FEEDFORWARD,
- *     RECURRENT,
- *     LSTM,
- *     PEEPHOLELSTM,
- *     GRAVESLSTM,
- *     GRU,
- *     MINGRU,
- *     CONVOLUTIONAL,
- *     POOLING
+ * Defines supported neural network layer types.
  *
  */
 public enum LayerType {
 
+    /**
+     * Feedforward layer
+     *
+     */
     FEEDFORWARD,
+
+    /**
+     * Vanilla recurrent layer
+     *
+     */
     RECURRENT,
+
+    /**
+     * Long Short Term Memory (LSTM) layer
+     *
+     */
     LSTM,
+
+    /**
+     * Peephole Long Short Term Memory (LSTM) layer
+     *
+     */
     PEEPHOLELSTM,
+
+    /**
+     * Graves style Long Short Term Memory (LSTM) layer
+     *
+     */
     GRAVESLSTM,
+
+    /**
+     * Gated Recurrent Unit (GRU) layer
+     *
+     */
     GRU,
+
+    /**
+     * Minimal Gated Recurrent Unit (GRU) layer
+     *
+     */
     MINGRU,
+
+    /**
+     * Convolutional layer implemented as convolution, crosscorrelation or minimal Winograd convolution
+     *
+     */
     CONVOLUTIONAL,
+
+    /**
+     * Max or average pooling layer
+     *
+     */
     POOLING
 
 }
