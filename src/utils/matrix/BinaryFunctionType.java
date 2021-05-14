c35a781cc19281cc3098b383afd4d7459cf01f6a
saaltone <simo.aaltonen@kolumbus.fi>
2021-05-14 23:14:35 +0300

    Cosmetic updates.

--- src/utils/matrix/BinaryFunctionType.java
+++ src/utils/matrix/BinaryFunctionType.java
@@ -6,48 +6,117 @@
 package utils.matrix;
 
 /**
- * Defines supported binary functions.<br>
- * <br>
- * Following functions are supported:
- *     MEAN_SQUARED_ERROR,
- *     MEAN_SQUARED_LOGARITHMIC_ERROR,
- *     MEAN_ABSOLUTE_ERROR,
- *     MEAN_ABSOLUTE_PERCENTAGE_ERROR,
- *     CROSS_ENTROPY,
- *     KULLBACK_LEIBLER,
- *     NEGATIVE_LOG_LIKELIHOOD,
- *     POISSON,
- *     HINGE,
- *     SQUARED_HINGE,
- *     HUBER,
- *     DIRECT_GRADIENT,
- *     POLICY_GRADIENT,
- *     POLICY_VALUE,
- *     POW,
- *     MAX,
- *     MIN,
- *     CUSTOM
+ * Defines supported binary functions.
  *
  */
 public enum BinaryFunctionType {
 
+    /**
+     * Mean squared error
+     *
+     */
     MEAN_SQUARED_ERROR,
+
+    /**
+     * Mean squared logarithmic error
+     *
+     */
     MEAN_SQUARED_LOGARITHMIC_ERROR,
+
+    /**
+     * Mean absolute error
+     *
+     */
     MEAN_ABSOLUTE_ERROR,
+
+    /**
+     * Mean absolute percentage error
+     *
+     */
     MEAN_ABSOLUTE_PERCENTAGE_ERROR,
+
+    /**
+     * Cross-entropy (log) error
+     *
+     */
     CROSS_ENTROPY,
+
+    /**
+     * Kullback-Leibler error
+     *
+     */
     KULLBACK_LEIBLER,
+
+    /**
+     * Negative log likelihood error
+     *
+     */
     NEGATIVE_LOG_LIKELIHOOD,
+
+    /**
+     * Poisson error
+     *
+     */
     POISSON,
+
+    /**
+     * Hinge error
+     *
+     */
     HINGE,
+
+    /**
+     * Squared hinge error
+     *
+     */
     SQUARED_HINGE,
+
+    /**
+     * Huber error
+     *
+     */
     HUBER,
+
+    /**
+     * Direct (user definable) gradient.
+     *
+     */
     DIRECT_GRADIENT,
+
+    /**
+     * Policy gradient
+     *
+     */
     POLICY_GRADIENT,
+
+    /**
+     * Policy value i.e. first entry contains value function error and rest policy gradient values for policy function.
+     *
+     */
     POLICY_VALUE,
+
+    /**
+     * Power function with definable power value
+     *
+     */
     POW,
+
+    /**
+     * Maximum between two entries
+     *
+     */
     MAX,
+
+    /**
+     * Minimum between two entries
+     *
+     */
     MIN,
+
+    /**
+     * Customer (user definable) function
+     *
+     */
     CUSTOM
 
 }
--- src/utils/matrix/UnaryFunctionType.java
+++ src/utils/matrix/UnaryFunctionType.java
@@ -6,83 +6,225 @@
 package utils.matrix;
 
 /**
- * Defines supported unary functions.<br>
- * <br>
- * Following functions are supported:
- *     ABS,
- *     COS,
- *     COSH,
- *     EXP,
- *     LOG,
- *     LOG10,
- *     SGN,
- *     SIN,
- *     SINH,
- *     SQRT,
- *     CBRT,
- *     MULINV,
- *     TAN,
- *     TANH,
- *     LINEAR,
- *     SIGMOID,
- *     SWISH,
- *     HARDSIGMOID,
- *     BIPOLARSIGMOID,
- *     TANHSIG,
- *     TANHAPPR,
- *     HARDTANH,
- *     SOFTPLUS,
- *     SOFTSIGN,
- *     RELU,
- *     RELU_COS,
- *     RELU_SIN,
- *     ELU,
- *     SELU,
- *     GELU,
- *     SOFTMAX,
- *     GUMBEL_SOFTMAX,
- *     GAUSSIAN,
- *     SINACT,
- *     LOGIT,
- *     CUSTOM
+ * Defines supported unary functions.
  *
  */
 public enum UnaryFunctionType {
+
+    /**
+     * Absolute
+     *
+     */
     ABS,
+
+    /**
+     * Cosine
+     *
+     */
     COS,
+
+    /**
+     * Hyperbolic cosine
+     *
+     */
     COSH,
+
+    /**
+     * Exponential
+     *
+     */
     EXP,
+
+    /**
+     * Natural logarithm
+     *
+     */
     LOG,
+
+    /**
+     * Base 10 logarithm
+     *
+     */
     LOG10,
+
+    /**
+     * Sign
+     *
+     */
     SGN,
+
+    /**
+     * Sine
+     *
+     */
     SIN,
+
+    /**
+     * Hyperbolic sine
+     *
+     */
     SINH,
+
+    /**
+     * Square root
+     *
+     */
     SQRT,
+
+    /**
+     * Cubic root
+     *
+     */
     CBRT,
+
+    /**
+     * Inverse multiplication
+     *
+     */
     MULINV,
+
+    /**
+     * Tangent
+     *
+     */
     TAN,
+
+    /**
+     * Hyperbolic tangent
+     *
+     */
     TANH,
+
+    /**
+     * Linear
+     *
+     */
     LINEAR,
+
+    /**
+     * Sigmoid
+     *
+     */
     SIGMOID,
+
+    /**
+     * Swish
+     *
+     */
     SWISH,
+
+    /**
+     * Hard sigmoid
+     *
+     */
     HARDSIGMOID,
+
+    /**
+     * Bi-polar sigmoid
+     *
+     */
     BIPOLARSIGMOID,
+
+    /**
+     * Hyperbolic tangent sigmoid
+     *
+     */
     TANHSIG,
+
+    /**
+     * Approximated tangent.
+     *
+     */
     TANHAPPR,
+
+    /**
+     * Hard tangent
+     *
+     */
     HARDTANH,
+
+    /**
+     * Softplus
+     *
+     */
     SOFTPLUS,
+
+    /**
+     * Softsign
+     *
+     */
     SOFTSIGN,
+
+    /**
+     * Rectified Linear Unit (ReLU)
+     *
+     */
     RELU,
+
+    /**
+     * Cosine ReLU
+     *
+     */
     RELU_COS,
+
+    /**
+     * Sine ReLU
+     *
+     */
     RELU_SIN,
+
+    /**
+     * Exponential Linear Unit (ELU)
+     *
+     */
     ELU,
+
+    /**
+     * Scaled Exponential Linear Unit (SELU)
+     *
+     */
     SELU,
+
+    /**
+     * Gaussian Error Linear Units (GELU)
+     *
+     */
     GELU,
+
+    /**
+     * Softmax
+     *
+     */
     SOFTMAX,
+
+    /**
+     * Gumbel Softmax
+     *
+     */
     GUMBEL_SOFTMAX,
+
+    /**
+     * Gaussian
+     *
+     */
     GAUSSIAN,
+
+    /**
+     * Sine activation
+     *
+     */
     SINACT,
+
+    /**
+     * Logit
+     *
+     */
     LOGIT,
+
+    /**
+     * Custom (user definable) function
+     *
+     */
     CUSTOM
 
 }
