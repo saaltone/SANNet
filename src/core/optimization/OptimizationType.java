cf99cc0478081a72645646867c181fc880e6d753
saaltone <simo.aaltonen@kolumbus.fi>
2021-05-14 23:06:47 +0300

    Cosmetic updates.

--- src/core/optimization/OptimizationType.java
+++ src/core/optimization/OptimizationType.java
@@ -6,36 +6,81 @@
 package core.optimization;
 
 /**
- * Defines supported optimization types.<br>
- * <br>
- * Currently supported types are:
- *    GRADIENT_DESCENT,
- *    MOMENTUM_GRADIENT_DESCENT,
- *    NESTEROV_ACCELERATED_GRADIENT,
- *    ADAGRAD,
- *    ADADELTA,
- *    RMSPROP,
- *    ADAM,
- *    ADAMAX,
- *    NADAM,
- *    RADAM,
- *    AMSGRAD,
- *    RESILIENT_PROPAGATION
+ * Defines supported optimization types.
  *
  */
 public enum OptimizationType {
 
+    /**
+     * Gradient descent
+     *
+     */
     GRADIENT_DESCENT,
+
+    /**
+     * Momentum gradient descent
+     *
+     */
     MOMENTUM_GRADIENT_DESCENT,
+
+    /**
+     * Nesterov accelerated gradient
+     *
+     */
     NESTEROV_ACCELERATED_GRADIENT,
+
+    /**
+     * Adagrad
+     *
+     */
     ADAGRAD,
+
+    /**
+     * Adadelta
+     *
+     */
     ADADELTA,
+
+    /**
+     * RMPProp
+     *
+     */
     RMSPROP,
+
+    /**
+     * Adam
+     *
+     */
     ADAM,
+
+    /**
+     * Adamax
+     *
+     */
     ADAMAX,
+
+    /**
+     * NAdam
+     *
+     */
     NADAM,
+
+    /**
+     * RAdam
+     *
+     */
     RADAM,
+
+    /**
+     * AMSGrad
+     *
+     */
     AMSGRAD,
+
+    /**
+     * Resilient propagation
+     *
+     */
     RESILIENT_PROPAGATION
 
 }
