d023d36e3b10c9b734f564338a3603158791f622
saaltone <simo.aaltonen@kolumbus.fi>
2021-11-05 00:00:09 +0200

    Refactored BasicSampler code completely.

--- src/utils/sampling/BasicSampler.java
+++ src/utils/sampling/BasicSampler.java
@@ -6,9 +6,9 @@
 package utils.sampling;
 
 import core.network.NeuralNetworkException;
-import utils.DynamicParam;
-import utils.DynamicParamException;
-import utils.Sequence;
+import utils.configurable.Configurable;
+import utils.configurable.DynamicParam;
+import utils.configurable.DynamicParamException;
 import utils.matrix.MMatrix;
 
 import java.io.Serial;
@@ -19,10 +19,41 @@ import java.util.*;
  * Class that defines BasicSampler for neural network.<br>
  *
  */
-public class BasicSampler implements Sampler, Serializable {
+public class BasicSampler implements Sampler, Configurable, Serializable {
 
     @Serial
     private static final long serialVersionUID = 1745926046002213714L;
+
+    /**
+     * Sets parameters used for BasicSampler.<br>
+     * <br>
+     * Supported parameters are:<br>
+     *     - numberOfIterations: number of training or validation iterations executed during step. Default value 1.<br>
+     *     - perEpoch: if true sampling takes place epoch wise i.e. samples are removed from sample set until all samples have been sampled at least once. Default value false.<br>
+     *     - fullSet: if true samples entire input as single set. Default value false.<br>
+     *     - randomStart: if true samples at random start. Default value false.<br>
+     *     - randomStartAfterSteps: if greater than zero chooses random start after defined steps. Default value -1.<br>
+     *     - randomOrder: if true samples in random order. Default value true.<br>
+     *     - stepForward: if true samples sampling steps in forward order (not valid for randomOrder sampling). Default value true.<br>
+     *     - stepSize: number of steps taken forward or backward when sampling (not valid for randomOrder sampling). Default value 1.<br>
+     *     - shuffleSamples: if true shuffles sampled samples. Default value false.<br>
+     *     - sampleReverse: if true samples in reverse order (assumes no sample shuffling). Default value false.<br>
+     *     - sampleSize: number of samples sampled. Default value 1.<br>
+     *     - cyclical: if true considered sample set as cyclical. Default value false.<br>
+     *
+     */
+    private final static String paramNameTypes = "(numberOfIterations:iNT), " +
+            "(perEpoch:BOOLEAN), " +
+            "(fullSet:BOOLEAN), " +
+            "(randomStart:BOOLEAN), " +
+            "(randomStartAfterSteps:INT), " +
+            "(randomOrder:BOOLEAN), " +
+            "(stepForward:BOOLEAN), " +
+            "(stepSize:INT), " +
+            "(shuffleSamples:BOOLEAN), " +
+            "(sampleReverse:BOOLEAN), " +
+            "(sampleSize:INT), " +
+            "(cyclical:BOOLEAN)";
 
     /**
      * Input sample set for sampling.
@@ -37,61 +68,85 @@ public class BasicSampler implements Sampler, Serializable {
     private final LinkedHashMap<Integer, MMatrix> outputs = new LinkedHashMap<>();
 
     /**
+     * Input sample set for sampling.
+     *
+     */
+    private final HashSet<Integer> inputSampleSet = new HashSet<>();
+
+    /**
      * Number of validation cycles.
      *
      */
-    private int numberOfIterations = 1;
+    private int numberOfIterations;
 
     /**
-     * If true samples entire input as single set. Default value false.
+     * if true sampling takes place epoch wise i.e. samples are removed from sample set until all samples have been sampled at least once.
      *
      */
-    private boolean fullSet = false;
+    private boolean perEpoch;
 
     /**
-     * If true samples at random start. Default value false.
+     * If true samples entire input as single set.
      *
      */
-    private boolean randomStart = false;
+    private boolean fullSet;
 
     /**
-     * If true samples in random order. Default value true.
+     * If true samples at random start.
      *
      */
-    private boolean randomOrder = true;
+    private boolean randomStart;
 
     /**
-     * If true sample steps in forward order (not valid for randomOrder sampling). Default value true.
+     * If greater or equal to zero chooses random start after defined steps.
      *
      */
-    private boolean stepForward = true;
+    private int randomStartAfterSteps;
 
     /**
-     * Number of steps taken forward or backward when sampling (no valid for randomOrder sampling). Default value 1.
+     * Random start after steps count.
+     *
+     */
+    private int randomStartAfterStepsCount = 0;
+
+    /**
+     * If true samples in random order.
+     *
+     */
+    private boolean randomOrder;
+
+    /**
+     * If true sample steps in forward order (not valid for randomOrder sampling)-
+     *
+     */
+    private boolean stepForward;
+
+    /**
+     * Number of steps taken forward or backward when sampling (no valid for randomOrder sampling).
      *
      */
     private int stepSize = 1;
 
     /**
-     * If true shuffles sampled samples. Default value false.
+     * If true shuffles sampled samples.
      *
      */
     private boolean shuffleSamples = false;
 
     /**
-     * If true samples in reverse order (assumes no sample shuffling). Default value false.
+     * If true samples in reverse order (assumes no sample shuffling).
      *
      */
     private boolean sampleReverse = false;
 
     /**
-     * Number of samples sampled. Default value 1.
+     * Number of samples sampled.
      *
      */
     private int sampleSize = 1;
 
     /**
-     * If true considers input samples as cyclical. Default value false.
+     * If true considers input samples as cyclical.
      *
      */
     private boolean cyclical = false;
@@ -122,6 +177,7 @@ public class BasicSampler implements Sampler, Serializable {
      * @throws NeuralNetworkException throws exception if input and output set sizes are not equal or not defined.
      */
     public BasicSampler(LinkedHashMap<Integer, MMatrix> inputs, LinkedHashMap<Integer, MMatrix> outputs) throws NeuralNetworkException {
+        initializeDefaultParams();
         if (inputs == null || outputs == null) throw new NeuralNetworkException("Inputs or outputs are not defined.");
         if (inputs.isEmpty() || outputs.isEmpty()) throw new NeuralNetworkException("Input and output data sets cannot be empty.");
         if (inputs.size() != outputs.size()) throw new NeuralNetworkException("Size of sample inputs and outputs must match.");
@@ -140,7 +196,26 @@ public class BasicSampler implements Sampler, Serializable {
      */
     public BasicSampler(LinkedHashMap<Integer, MMatrix> inputs, LinkedHashMap<Integer, MMatrix> outputs, String params) throws NeuralNetworkException, DynamicParamException {
         this(inputs, outputs);
-        setParams(new DynamicParam(params, getParamDefs()));
+        if (params != null) setParams(new DynamicParam(params, getParamDefs()));
+    }
+
+    /**
+     * Initializes default params.
+     *
+     */
+    public void initializeDefaultParams() {
+        numberOfIterations = 1;
+        perEpoch = false;
+        fullSet = false;
+        randomStart = false;
+        randomOrder = true;
+        randomStartAfterSteps = 0;
+        stepForward = true;
+        stepSize = 1;
+        shuffleSamples = false;
+        sampleReverse = false;
+        sampleSize = 1;
+        cyclical = false;
     }
 
     /**
@@ -148,19 +223,8 @@ public class BasicSampler implements Sampler, Serializable {
      *
      * @return parameters used for BasicSampler.
      */
-    protected HashMap<String, DynamicParam.ParamType> getParamDefs() {
-        HashMap<String, DynamicParam.ParamType> paramDefs = new HashMap<>();
-        paramDefs.put("numberOfIterations", DynamicParam.ParamType.INT);
-        paramDefs.put("fullSet", DynamicParam.ParamType.BOOLEAN);
-        paramDefs.put("randomOrder", DynamicParam.ParamType.BOOLEAN);
-        paramDefs.put("randomStart", DynamicParam.ParamType.BOOLEAN);
-        paramDefs.put("stepForward", DynamicParam.ParamType.BOOLEAN);
-        paramDefs.put("stepSize", DynamicParam.ParamType.INT);
-        paramDefs.put("shuffleSamples", DynamicParam.ParamType.BOOLEAN);
-        paramDefs.put("sampleReverse", DynamicParam.ParamType.BOOLEAN);
-        paramDefs.put("sampleSize", DynamicParam.ParamType.INT);
-        paramDefs.put("cyclical", DynamicParam.ParamType.BOOLEAN);
-        return paramDefs;
+    public String getParamDefs() {
+        return BasicSampler.paramNameTypes;
     }
 
     /**
@@ -168,8 +232,10 @@ public class BasicSampler implements Sampler, Serializable {
      * <br>
      * Supported parameters are:<br>
      *     - numberOfIterations: number of training or validation iterations executed during step. Default value 1.<br>
+     *     - perEpoch: if true sampling takes place epoch wise i.e. samples are removed from sample set until all samples have been sampled at least once. Default value false.<br>
      *     - fullSet: if true samples entire input as single set. Default value false.<br>
      *     - randomStart: if true samples at random start. Default value false.<br>
+     *     - randomStartAfterSteps: if greater than zero chooses random start after defined steps. Default value -1.<br>
      *     - randomOrder: if true samples in random order. Default value true.<br>
      *     - stepForward: if true samples sampling steps in forward order (not valid for randomOrder sampling). Default value true.<br>
      *     - stepSize: number of steps taken forward or backward when sampling (not valid for randomOrder sampling). Default value 1.<br>
@@ -186,9 +252,11 @@ public class BasicSampler implements Sampler, Serializable {
             numberOfIterations = params.getValueAsInteger("numberOfIterations");
             if (numberOfIterations < 1) throw new DynamicParamException("Number of iterations must be at least 1.");
         }
+        if (params.hasParam("perEpoch")) perEpoch = params.getValueAsBoolean("perEpoch");
         if (params.hasParam("fullSet")) fullSet = params.getValueAsBoolean("fullSet");
         if (params.hasParam("randomOrder")) randomOrder = params.getValueAsBoolean("randomOrder");
         if (params.hasParam("randomStart")) randomStart = params.getValueAsBoolean("randomStart");
+        if (params.hasParam("randomStartAfterSteps")) randomStartAfterSteps = params.getValueAsInteger("randomStartAfterSteps");
         if (params.hasParam("stepForward")) stepForward = params.getValueAsBoolean("stepForward");
         if (params.hasParam("stepSize")) {
             stepSize = params.getValueAsInteger("stepSize");
@@ -258,54 +326,87 @@ public class BasicSampler implements Sampler, Serializable {
     public void getSamples(Sequence inputSequence, Sequence outputSequence) throws NeuralNetworkException {
         if (inputSequence.getDepth() != outputSequence.getDepth()) throw new NeuralNetworkException("Depth of samples input and output sequences must match");
 
-        ArrayList<Integer> sampleIndices = new ArrayList<>();
-
-        int maxSampleSize;
-        if (fullSet) {
-            sampleAt = 0;
-            maxSampleSize = inputs.size();
-        }
-        else maxSampleSize = Math.min(sampleSize, inputs.size());
-
-        if (randomOrder) {
-            for (int index = 0; index < maxSampleSize; index++) {
-                sampleIndices.add(random.nextInt(inputs.size() - (cyclical ? 1 : maxSampleSize) + 1));
-            }
-        }
-        else {
-            if (randomStart) sampleAt = random.nextInt(inputs.size() - (cyclical ? 1 : maxSampleSize) + 1);
-            int sampleAtIndex = !fullSet ? sampleAt : 0;
-            for (int index = 0; index < maxSampleSize; index++) {
-                sampleIndices.add(sampleAtIndex);
-                if (sampleReverse) {
-                    sampleAtIndex--;
-                    sampleAtIndex = sampleAtIndex < 0 ? inputs.size() - 1 : sampleAtIndex;
-                }
-                else {
-                    sampleAtIndex++;
-                    sampleAtIndex = sampleAtIndex > inputs.size() - 1 ? 0 : sampleAtIndex;
-                }
-            }
-        }
-
-        if (shuffleSamples && maxSampleSize > 1) Collections.shuffle(sampleIndices);
+        ArrayList<Integer> sampleIndices = getSampleIndices();
 
         for (Integer sampleIndex : sampleIndices) {
             inputSequence.put(sampleIndex, inputs.get(sampleIndex));
             outputSequence.put(sampleIndex, outputs.get(sampleIndex));
         }
 
-        if (!randomOrder && !randomStart && !fullSet) {
-            if (stepForward) {
-                sampleAt += stepSize;
-                sampleAt = sampleAt > inputs.size() - (cyclical ? 1 : maxSampleSize) ? 0 : sampleAt;
-            }
-            else {
-                sampleAt -= stepSize;
-                sampleAt = sampleAt < 0  ? inputs.size() - (cyclical ? 1 : maxSampleSize) : sampleAt;
+    }
+
+    /**
+     * Returns sampled indices following sampling rules.
+     *
+     * @return sampled indices.
+     */
+    private ArrayList<Integer> getSampleIndices() {
+        if (inputSampleSet.isEmpty()) inputSampleSet.addAll(inputs.keySet());
+
+        ArrayList<Integer> sampleIndices = new ArrayList<>();
+        if (randomOrder) {
+            ArrayList<Integer> inputSamples = new ArrayList<>(inputSampleSet);
+            Collections.shuffle(inputSamples);
+            int maxSampleAmount = Math.min(sampleSize, inputSamples.size());
+            for (int sampleIndex = 0; sampleIndex < maxSampleAmount; sampleIndex++) {
+                sampleIndices.add(inputSamples.get(sampleIndex));
             }
         }
+        else {
+            if (fullSet) sampleIndices.addAll(inputSampleSet);
+            else {
+                if (randomStart || (randomStartAfterSteps > 0 && ++randomStartAfterSteps >= randomStartAfterStepsCount)) {
+                    if (cyclical) sampleAt = random.nextInt(inputSampleSet.size());
+                    else {
+                        if (!stepForward) sampleAt = Math.min(0, random.nextInt(inputSampleSet.size() - sampleSize + 1));
+                        else sampleAt = Math.max(0, random.nextInt(inputSampleSet.size()) - sampleSize);
+                    }
+                }
+                if (randomStartAfterSteps > 0 && randomStartAfterStepsCount > randomStartAfterSteps) randomStartAfterStepsCount = 0;
+                int maxSampleAmount = Math.min(sampleSize, inputSampleSet.size());
+                int sampleIndex = sampleAt;
+                for (int sampleCount = 0; sampleCount < maxSampleAmount; sampleCount++) {
+                    sampleIndices.add(sampleIndex);
+                    sampleIndex += stepForward ? -1 : 1;
+                    if (!stepForward) {
+                        if (sampleIndex > inputSampleSet.size() - 1) {
+                            if (!cyclical) break;
+                            sampleIndex = 0;
+                        }
+                    }
+                    else {
+                        if (sampleIndex < 0) {
+                            if (!cyclical) break;
+                            sampleIndex = inputSampleSet.size() - 1;
+                        }
+                    }
+                }
+                sampleAt += stepForward ? -stepSize : stepSize;
+                if (cyclical) {
+                    if (!stepForward) {
+                        if (sampleAt > inputSampleSet.size() - 1) sampleAt = 0;
+                    }
+                    else {
+                        if (sampleAt < 0) sampleAt = inputSampleSet.size() - 1;
+                    }
+                }
+                else {
+                    if (!stepForward) {
+                        if (sampleAt > inputSampleSet.size() - sampleSize) sampleAt = 0;
+                    }
+                    else {
+                        if (sampleAt < sampleSize) sampleAt = inputSampleSet.size() - 1;
+                    }
+                }
+            }
 
+            if (shuffleSamples) Collections.shuffle(sampleIndices);
+            else if (sampleReverse) Collections.reverse(sampleIndices);
+
+            if (perEpoch) for (Integer sampleIndex : sampleIndices) inputSampleSet.remove(sampleIndex);
+        }
+
+        return sampleIndices;
     }
 
 }
