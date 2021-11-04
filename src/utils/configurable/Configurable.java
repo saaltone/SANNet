d4d6ddffbfb1152d0e3072be508ef11eaa26882d
saaltone <simo.aaltonen@kolumbus.fi>
2021-11-05 00:02:54 +0200

    Refactored configurable parameter handling into dedicated package.

---
+++ src/utils/configurable/Configurable.java
@@ -0,0 +1,36 @@
+/*
+ * SANNet Neural Network Framework
+ * Copyright (C) 2018 - 2021 Simo Aaltonen
+ */
+
+package utils.configurable;
+
+/**
+ * Interface that defines configurable entity.<br>
+ *
+ */
+public interface Configurable {
+
+    /**
+     * Initializes default params.
+     *
+     * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     */
+    void initializeDefaultParams() throws DynamicParamException;
+
+    /**
+     * Returns parameters used for configurable entity.
+     *
+     * @return parameters used for configurable entity.
+     */
+    String getParamDefs();
+
+    /**
+     * Sets parameters used for configurable entity.<br>
+     *
+     * @param params parameters
+     * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     */
+    void setParams(DynamicParam params) throws DynamicParamException;
+
+}
---
+++ src/utils/configurable/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package that defines configurable parameters.
+ *
+ */
+package utils.configurable;
