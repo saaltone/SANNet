8fd0842dbca21288c6cc50879d33a9e4a3da6c58
saaltone <simo.aaltonen@kolumbus.fi>
2021-11-07 21:58:32 +0200

    Added random pooling layer.

---
+++ src/core/layer/convolutional/RandomPoolingLayer.java
@@ -0,0 +1,56 @@
+/*
+ * SANNet Neural Network Framework
+ * Copyright (C) 2018 - 2021 Simo Aaltonen
+ */
+
+package core.layer.convolutional;
+
+import core.network.NeuralNetworkException;
+import utils.configurable.DynamicParamException;
+import utils.matrix.Initialization;
+import utils.matrix.Matrix;
+import utils.matrix.MatrixException;
+
+import java.util.HashMap;
+
+/**
+ * Defines random pooling layer.<br>
+ * Selects each input of pool for propagation randomly with uniform probability.<br>
+ *
+ */
+public class RandomPoolingLayer extends AbstractPoolingLayer {
+
+    /**
+     * Constructor for RandomPoolingLayer.
+     *
+     * @param layerIndex layer Index.
+     * @param initialization initialization function for weight maps (not relevant for pooling layer).
+     * @param params parameters for pooling layer.
+     * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws NeuralNetworkException throws exception setting of activation function fails.
+     */
+    public RandomPoolingLayer(int layerIndex, Initialization initialization, String params) throws DynamicParamException, NeuralNetworkException {
+        super (layerIndex, initialization, params);
+    }
+
+    /**
+     * Executes pooling operation.
+     *
+     * @param input input matrix.
+     * @param output output matrix.
+     * @throws MatrixException throws exception if matrix operation fails.
+     */
+    protected void executePoolingOperation(Matrix input, Matrix output) throws MatrixException {
+        input.randomPool(output, new HashMap<>());
+    }
+
+    /**
+     * Returns pooling type.
+     *
+     * @return pooling type.
+     */
+    protected String getPoolingType() {
+        return "Random";
+    }
+
+}
