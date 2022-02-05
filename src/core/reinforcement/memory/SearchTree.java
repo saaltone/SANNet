da35c8d886b51210a17fbf9d3fedf242f3ff4ab7
saaltone <simo.aaltonen@kolumbus.fi>
2022-02-05 18:41:45 +0200

    Added interface for SearchTree used by PriorityMemory.

---
+++ src/core/reinforcement/memory/SearchTree.java
@@ -0,0 +1,65 @@
+/*
+ * SANNet Neural Network Framework
+ * Copyright (C) 2018 - 2022 Simo Aaltonen
+ */
+
+package core.reinforcement.memory;
+
+/**
+ * Interface for search tree.
+ *
+ */
+public interface SearchTree {
+
+    /**
+     * Current size of search tree i.e. number of state transitions stored.
+     *
+     * @return size of search tree.
+     */
+    int size();
+
+    /**
+     * Returns total capacity of search tree.
+     *
+     * @return total capacity of search tree.
+     */
+    int capacity();
+
+    /**
+     * Returns total priority of search tree.
+     *
+     * @return total priority of search tree.
+     */
+    double getTotalPriority();
+
+    /**
+     * Adds state transition in search tree at the location of current node and shifts current node one forward.
+     * Updates total priority of search tree according to priority of added state transition.
+     *
+     * @param stateTransition state transition to be added.
+     */
+    void add(StateTransition stateTransition);
+
+    /**
+     * Updates priority of state transition and entire search tree.
+     *
+     * @param stateTransition state transition to be updated.
+     */
+    void update(StateTransition stateTransition);
+
+    /**
+     * Returns state transition by priority sum.
+     *
+     * @param prioritySum priority sum.
+     * @return state transition according to priority sum.
+     */
+    StateTransition getStateTransition(double prioritySum);
+
+    /**
+     * Returns random state transition.
+     *
+     * @return random state transition.
+     */
+    StateTransition getRandomStateTransition();
+
+}
