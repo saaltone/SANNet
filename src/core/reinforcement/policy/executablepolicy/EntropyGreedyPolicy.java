0bb2dcfcca162b7ecf58e105fea05d2130ec6bb8
saaltone <simo.aaltonen@kolumbus.fi>
2022-11-11 22:51:57 +0200

    Added entropy greedy policy.

---
+++ src/core/reinforcement/policy/executablepolicy/EntropyGreedyPolicy.java
@@ -0,0 +1,46 @@
+/*
+ * SANNet Neural Network Framework
+ * Copyright (C) 2018 - 2022 Simo Aaltonen
+ */
+
+package core.reinforcement.policy.executablepolicy;
+
+import java.util.Random;
+import java.util.TreeSet;
+
+/**
+ * Implements entropy greedy policy.<br>
+ *
+ */
+public class EntropyGreedyPolicy extends GreedyPolicy {
+
+    /**
+     * Random function for entropy greedy policy.
+     *
+     */
+    private final Random random = new Random();
+
+    /**
+     * Constructor for entropy greedy policy.
+     *
+     */
+    public EntropyGreedyPolicy() {
+        super(ExecutablePolicyType.ENTROPY_GREEDY);
+    }
+
+    /**
+     * Returns action based on policy.
+     *
+     * @param stateValueSet priority queue containing action values in decreasing order.
+     * @return chosen action.
+     */
+    protected int getAction(TreeSet<AbstractExecutablePolicy.ActionValueTuple> stateValueSet) {
+        if (Math.random() < getActionEntropy(stateValueSet)) {
+            AbstractExecutablePolicy.ActionValueTuple[] actionValueTupleArray = new AbstractExecutablePolicy.ActionValueTuple[stateValueSet.size()];
+            actionValueTupleArray = stateValueSet.toArray(actionValueTupleArray);
+            return actionValueTupleArray[random.nextInt(actionValueTupleArray.length)].action();
+        }
+        else return super.getAction(stateValueSet);
+    }
+
+}
