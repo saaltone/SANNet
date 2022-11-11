91b649d6e230ef3f7cb31ee8cf5f6d1b509cc710
saaltone <simo.aaltonen@kolumbus.fi>
2022-11-11 22:58:21 +0200

    Minor updates.

--- src/core/layer/package-info.java
+++ src/core/layer/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Defines functionalities for input, hidden and output layers.<br>
+ * Includes layers for regularization and normalization.<br>
+ * Includes layers for reinforcement learning.<br>
  * Enables layer to run as thread to enable concurrency of multiple neural network instances.<br>
  *
  */
