e336a3d5173af6832dd949b1896e9c4ebfde0267
saaltone <simo.aaltonen@kolumbus.fi>
2022-11-11 22:59:37 +0200

    Minor update.

--- src/core/layer/recurrent/package-info.java
+++ src/core/layer/recurrent/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Defines recurrent (vanilla, LSTM and GRU) layers.<br>
+ * Defines recurrent (vanilla, LSTM and GRU) layers with option for bi-directionality.<br>
  *
  */
 package core.layer.recurrent;
