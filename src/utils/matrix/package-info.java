dd8c00d2f4460774f0b013aed0c9fe6511687dda
saaltone <simo.aaltonen@kolumbus.fi>
2022-11-11 22:55:48 +0200

    Minor update.

--- src/utils/matrix/package-info.java
+++ src/utils/matrix/package-info.java
@@ -2,7 +2,7 @@
  * Defines matrix functions for neural network.<br>
  * Provides dense and sparse matrices with masking.<br>
  * Provides versatile set of matrix functions for calculation including unary and binary functions.<br>
- * Provides multi-matrix to handle multiple matrix at a time.<br>
+ * Provides multi-matrix to handle multiple matrices at a time.<br>
  *
  */
 package utils.matrix;
