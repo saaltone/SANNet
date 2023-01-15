5429f37bd3f96aaff7804cd67a9331f9ab33bf90
saaltone <simo.aaltonen@kolumbus.fi>
2023-01-15 20:24:33 +0200

    Added mentioning of time series demo.

--- src/demo/package-info.java
+++ src/demo/package-info.java
@@ -4,7 +4,7 @@
  *  - AutoEncoder demo that applies auto encoder type neural network.<br>
  *  - MNIST demo that applies convolutional neural network.<br>
  *  - Simple demo that applies feedforward neural network.<br>
- *  - Text sequence and music demos that apply recurrent neural network.<br>
+ *  - Text sequence, time series and music demos that apply recurrent neural network.<br>
  *  - Maze, Tic Tac Toe and travelling salesman problem (TSP) demos that apply reinforcement learning.<br>
  *
  */
