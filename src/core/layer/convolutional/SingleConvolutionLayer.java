70d95163661513dde8b084614e5fa6d701319cb0
Simo Aaltonen <simo.aaltonen@kolumbus.fi>
2023-09-08 23:51:17 +0300

    Minor updates.

--- src/core/layer/convolutional/SingleConvolutionLayer.java
+++ src/core/layer/convolutional/SingleConvolutionLayer.java
@@ -6,7 +6,6 @@
 package core.layer.convolutional;
 
 import core.network.NeuralNetworkException;
-import utils.configurable.DynamicParam;
 import utils.configurable.DynamicParamException;
 import utils.matrix.Initialization;
 import utils.matrix.Matrix;
@@ -19,22 +18,6 @@ import utils.matrix.MatrixException;
 public class SingleConvolutionLayer extends AbstractSingleConvolutionalLayer {
 
     /**
-     * Parameter name types for single convolution layer.
-     *     - filterSize size of filter. Default value 3.<br>
-     *     - filterRowSize size of filter in terms of rows. Overrides filterSize parameter. Default value 3.<br>
-     *     - filterColumnSize size of filter in terms of columns. Overrides filterSize parameter. Default value 3.<br>
-     *     - stride: size of stride. Default size 1.<br>
-     *     - dilation: dilation step for filter. Default step 1.<br>
-     *
-     */
-    private final static String paramNameTypes = "(filterSize:INT), " +
-            "(filterRowSize:INT), " +
-            "(filterColumnSize:INT), " +
-            "(stride:INT), " +
-            "(dilation:INT), ";
-
-
-    /**
      * Constructor for single convolution layer.
      *
      * @param layerIndex layer index
@@ -45,59 +28,6 @@ public class SingleConvolutionLayer extends AbstractSingleConvolutionalLayer {
      */
     public SingleConvolutionLayer(int layerIndex, Initialization initialization, String params) throws DynamicParamException, NeuralNetworkException {
         super (layerIndex, initialization, params);
-    }
-
-    /**
-     * Returns parameters used for single convolution layer.
-     *
-     * @return parameters used for single convolution layer.
-     */
-    public String getParamDefs() {
-        return super.getParamDefs() + ", " + SingleConvolutionLayer.paramNameTypes;
-    }
-
-    /**
-     * Sets parameters used for single convolution layer.<br>
-     * <br>
-     * Supported parameters are:<br>
-     *     - filterSize size of filter. Default value 3.<br>
-     *     - filterRowSize size of filter in terms of rows. Overrides filterSize parameter. Default value 3.<br>
-     *     - filterColumnSize size of filter in terms of columns. Overrides filterSize parameter. Default value 3.<br>
-     *     - stride: size of stride. Default size 1.<br>
-     *     - dilation: dilation step for filter. Default step 1.<br>
-     *
-     * @param params parameters used for single convolution layer.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     * @throws NeuralNetworkException throws exception if minimum layer dimensions are not met.
-     */
-    public void setParams(DynamicParam params) throws DynamicParamException, NeuralNetworkException {
-        super.setParams(params);
-        if (params.hasParam("filterSize")) {
-            int filterSize = params.getValueAsInteger("filterSize");
-            if (filterSize < 1) throw new NeuralNetworkException("Filter size must be at least 1.");
-            setFilterRowSize(filterSize);
-            setFilterColumnSize(filterSize);
-        }
-        if (params.hasParam("filterRowSize")) {
-            int filterRowSize = params.getValueAsInteger("filterRowSize");
-            if (filterRowSize < 1) throw new NeuralNetworkException("Filter row size must be at least 1.");
-            setFilterRowSize(filterRowSize);
-        }
-        if (params.hasParam("filterColumnSize")) {
-            int filterColumnSize = params.getValueAsInteger("filterColumnSize");
-            if (filterColumnSize < 1) throw new NeuralNetworkException("Filter column size must be at least 1.");
-            setFilterColumnSize(filterColumnSize);
-        }
-        if (params.hasParam("stride")) {
-            int stride = params.getValueAsInteger("stride");
-            if (stride < 1) throw new NeuralNetworkException("Stride must be at least 1.");
-            setStride(stride);
-        }
-        if (params.hasParam("dilation")) {
-            int dilation = params.getValueAsInteger("dilation");
-            if (dilation < 1) throw new NeuralNetworkException("Dilation must be at least 1.");
-            setDilation(dilation);
-        }
     }
 
     /**
