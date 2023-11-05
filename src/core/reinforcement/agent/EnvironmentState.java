a8a41454982a0305b5a72289ff4e76bcc59445e1
Simo Aaltonen <simo.aaltonen@kolumbus.fi>
2023-11-05 21:23:22 +0200

    Refactored episode id and time step into State and StateSynchronization.

--- src/core/reinforcement/agent/EnvironmentState.java
+++ src/core/reinforcement/agent/EnvironmentState.java
@@ -14,36 +14,20 @@ import java.util.HashSet;
 /**
  * Record that defines state of environment.
  *
- * @param episodeID episode ID.
- * @param timeStep time step.
  * @param state state of environment.
  * @param availableActions actions available at state.
  *
  */
-public record EnvironmentState(int episodeID, int timeStep, Matrix state, HashSet<Integer> availableActions) implements Serializable, Comparable<EnvironmentState> {
+public record EnvironmentState(Matrix state, HashSet<Integer> availableActions) implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -3840329155579749639L;
-
-    /**
-     * Compares this environment state to other environment state.<br>
-     * If other environment state is precedent to this environment state returns 1.<br>
-     * If other environment state succeeds this environment state returns -1.<br>
-     * If above conditions are not met returns 0.<br>
-     *
-     * @param otherEnvironmentState other environment state
-     * @return return value of comparison.
-     */
-    public int compareTo(EnvironmentState otherEnvironmentState) {
-        return episodeID > otherEnvironmentState.episodeID ? 1 : episodeID < otherEnvironmentState.episodeID ? -1 : Integer.compare(timeStep, otherEnvironmentState.timeStep);
-    }
 
     /**
      * Prints environment state.
      *
      */
     public void print() {
-        System.out.println("Episode ID: " + episodeID + " Time step: " + timeStep);
         state.print();
         System.out.println(availableActions);
     }
