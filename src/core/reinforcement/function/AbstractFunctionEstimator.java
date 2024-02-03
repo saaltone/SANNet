a7009b1476c51412872ea5391076cdcecc28ae4c
Simo Aaltonen <simo.aaltonen@kolumbus.fi>
2024-02-03 18:22:41 +0200

    Minor updates.

--- src/core/reinforcement/function/AbstractFunctionEstimator.java
+++ src/core/reinforcement/function/AbstractFunctionEstimator.java
@@ -7,17 +7,14 @@ package core.reinforcement.function;
 
 import core.reinforcement.agent.Agent;
 import core.reinforcement.agent.AgentException;
-import core.reinforcement.memory.Memory;
-import core.reinforcement.agent.State;
 import utils.configurable.Configurable;
-import utils.configurable.DynamicParam;
 import utils.configurable.DynamicParamException;
 import utils.matrix.Matrix;
+import utils.matrix.MatrixException;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.TreeSet;
 
 /**
  * Implements abstract function estimator containing memory management operations and agent handling.<br>
@@ -47,12 +44,6 @@ public abstract class AbstractFunctionEstimator implements Configurable, Functio
     private final HashSet<Agent> completedAgents = new HashSet<>();
 
     /**
-     * Memory instance used by function estimator.
-     *
-     */
-    protected final Memory memory;
-
-    /**
      * If true function is combined state action value function.
      *
      */
@@ -73,34 +64,32 @@ public abstract class AbstractFunctionEstimator implements Configurable, Functio
     /**
      * Constructor for abstract function estimator.
      *
-     * @param memory                     memory reference.
      * @param numberOfStates             number of states.
      * @param numberOfActions            number of actions.
      * @param isStateActionValueFunction if true function is combined state action value function.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws MatrixException throws exception if matrix operation fails.
      */
-    public AbstractFunctionEstimator(Memory memory, int numberOfStates, int numberOfActions, boolean isStateActionValueFunction) throws DynamicParamException {
-        this(memory, numberOfStates, numberOfActions, isStateActionValueFunction, null);
+    public AbstractFunctionEstimator(int numberOfStates, int numberOfActions, boolean isStateActionValueFunction) throws DynamicParamException, MatrixException {
+        this(numberOfStates, numberOfActions, isStateActionValueFunction, null);
     }
 
     /**
      * Constructor for abstract function estimator.
      *
-     * @param memory                     memory reference.
      * @param numberOfStates             number of states.
      * @param numberOfActions            number of actions.
      * @param isStateActionValueFunction if true function is combined state action value function.
      * @param params                     parameters for function
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws MatrixException throws exception if matrix operation fails.
      */
-    public AbstractFunctionEstimator(Memory memory, int numberOfStates, int numberOfActions, boolean isStateActionValueFunction, String params) throws DynamicParamException {
+    public AbstractFunctionEstimator(int numberOfStates, int numberOfActions, boolean isStateActionValueFunction, String params) throws DynamicParamException, MatrixException {
         initializeDefaultParams();
-        this.memory = memory;
         this.numberOfStates = numberOfStates;
         this.numberOfActions = numberOfActions;
         this.isStateActionValueFunction = isStateActionValueFunction;
         this.params = params;
-        if (params != null) setParams(new DynamicParam(params, getParamDefs()));
     }
 
     /**
@@ -110,37 +99,6 @@ public abstract class AbstractFunctionEstimator implements Configurable, Functio
      */
     protected String getParams() {
         return params;
-    }
-
-    /**
-     * Returns parameters used for abstract function estimator.
-     *
-     * @return parameters used for abstract function estimator.
-     */
-    public String getParamDefs() {
-        return memory.getParamDefs();
-    }
-
-    /**
-     * Sets parameters used for abstract function estimator.<br>
-     * <br>
-     * Supported parameters are:<br>
-     *     - targetFunctionUpdateCycle; target function update cycle. Default value 0 (smooth update).<br>
-     *
-     * @param params parameters used for abstract function estimator.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     */
-    public void setParams(DynamicParam params) throws DynamicParamException {
-        memory.setParams(params);
-    }
-
-    /**
-     * Returns reference to memory of function estimator.
-     *
-     * @return reference to memory of function estimator.
-     */
-    public Memory getMemory() {
-        return memory;
     }
 
     /**
@@ -171,45 +129,11 @@ public abstract class AbstractFunctionEstimator implements Configurable, Functio
     }
 
     /**
-     * Resets function estimator.
-     *
-     */
-    public void reset() {
-        memory.reset();
-    }
-
-    /**
-     * Samples memory of abstract function estimator.
-     *
-     */
-    public void sample() {
-        memory.sample();
-    }
-
-    /**
-     * Returns sampled states.
-     *
-     * @return sampled states.
-     */
-    public TreeSet<State> getSampledStates() {
-        return memory.getSampledStates();
-    }
-
-    /**
-     * Adds new state into memory of abstract function estimator.
-     *
-     * @param state state
-     */
-    public void add(State state) {
-        memory.add(state);
-    }
-
-    /**
      * Notifies that agent is ready to update.
      *
      * @param agent current agent.
-     * @throws AgentException throws exception if agent is not registered for function estimator.
      * @return true if all registered agents are ready to update.
+     * @throws AgentException throws exception if agent is not registered for function estimator.
      */
     public boolean readyToUpdate(Agent agent) throws AgentException {
         if (!registeredAgents.contains(agent)) throw new AgentException("Agent is not registered for function estimator.");
@@ -218,29 +142,18 @@ public abstract class AbstractFunctionEstimator implements Configurable, Functio
     }
 
     /**
-     * Updates states in memory of abstract function estimator.
-     *
-     * @param states states
-     */
-    public void update(TreeSet<State> states) {
-        memory.update(states);
-    }
-
-    /**
-     * Aborts function estimator update.
-     *
-     */
-    public void abortUpdate() {
-        completedAgents.clear();
-    }
-
-    /**
      * Completes abstract function estimator update.
+     */
+    public void updateComplete() {
+        completedAgents.clear();
+        reset();
+    }
+
+    /**
+     * Resets function estimator.
      *
      */
-    protected void updateComplete() {
-        completedAgents.clear();
-    }
+    protected abstract void reset();
 
     /**
      * If true value function is combined state action value function.
@@ -252,26 +165,17 @@ public abstract class AbstractFunctionEstimator implements Configurable, Functio
     }
 
     /**
-     * Returns max value of state given available actions.
-     *
-     * @param stateValues state values.
-     * @param availableActions actions available in state.
-     * @return max value of state.
-     */
-    public double max(Matrix stateValues, HashSet<Integer> availableActions) {
-        return stateValues.getValue(argmax(stateValues, availableActions), 0, 0);
-    }
-
-    /**
      * Returns action with maximum state value given available actions.
      *
      * @param stateValues state values.
      * @param availableActions actions available in state.
      * @return action with maximum state value.
+     * @throws MatrixException throws exception if matrix operation fails.
      */
-    public int argmax(Matrix stateValues, HashSet<Integer> availableActions) {
+    public int argmax(Matrix stateValues, HashSet<Integer> availableActions) throws MatrixException {
         int maxAction = -1;
         double maxValue = Double.NEGATIVE_INFINITY;
+        if (availableActions == null) return stateValues.argmax()[0];
         for (int action : availableActions) {
             double actionValue = stateValues.getValue(action, 0, 0);
             if (maxValue == Double.NEGATIVE_INFINITY || maxValue < actionValue) {
