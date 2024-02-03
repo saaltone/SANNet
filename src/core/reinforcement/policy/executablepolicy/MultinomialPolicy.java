095f28858e20b7d24eb40477f4aa504127e03c3c
Simo Aaltonen <simo.aaltonen@kolumbus.fi>
2024-02-03 17:59:31 +0200

    Refactored functionality of multinomial policy.

--- src/core/reinforcement/policy/executablepolicy/MultinomialPolicy.java
+++ src/core/reinforcement/policy/executablepolicy/MultinomialPolicy.java
@@ -5,10 +5,9 @@
 
 package core.reinforcement.policy.executablepolicy;
 
-import utils.configurable.DynamicParam;
 import utils.configurable.DynamicParamException;
+import utils.matrix.MatrixException;
 
-import java.util.Objects;
 import java.util.Random;
 import java.util.TreeSet;
 
@@ -19,65 +18,20 @@ import java.util.TreeSet;
 public class MultinomialPolicy extends AbstractExecutablePolicy {
 
     /**
-     * Parameter name types for multinomial policy.
-     *     - numberOfTrials: number of trials. Default value 1.<br>
-     *
-     */
-    private final static String paramNameTypes = "(numberOfTrials:INT)";
-
-    /**
      * Random function for multinomial policy.
      *
      */
     private final Random random = new Random();
 
     /**
-     * Number of trials
-     *
-     */
-    private int numberOfTrials;
-
-    /**
      * Constructor for multinomial policy.
      *
      * @param params parameters for multinomial policy.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
+     * @throws MatrixException throws exception if matrix operation fails.
      */
-    public MultinomialPolicy(String params) throws DynamicParamException {
-        super(ExecutablePolicyType.MULTINOMIAL, params, MultinomialPolicy.paramNameTypes);
-    }
-
-    /**
-     * Initializes default params.
-     *
-     */
-    public void initializeDefaultParams() {
-        super.initializeDefaultParams();
-        numberOfTrials = 1;
-    }
-
-    /**
-     * Returns parameters used for multinomial policy.
-     *
-     * @return parameters used for multinomial policy.
-     */
-    public String getParamDefs() {
-        return super.getParamDefs() + ", " + MultinomialPolicy.paramNameTypes;
-    }
-
-    /**
-     * Sets parameters used for multinomial policy.<br>
-     * <br>
-     * Supported parameters are:<br>
-     *     - numberOfTrials: number of trials. Default value 1.<br>
-     *
-     * @param params parameters used for multinomial policy.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     */
-    public void setParams(DynamicParam params) throws DynamicParamException {
-        super.setParams(params);
-        if (params.hasParam("numberOfTrials")) numberOfTrials = params.getValueAsInteger("numberOfTrials");
-        if (numberOfTrials < 1) throw new DynamicParamException("Number of trials cannot be less than 1.");
+    public MultinomialPolicy(String params) throws DynamicParamException, MatrixException {
+        super(ExecutablePolicyType.MULTINOMIAL, params, null);
     }
 
     /**
@@ -94,65 +48,28 @@ public class MultinomialPolicy extends AbstractExecutablePolicy {
      * @return chosen action.
      */
     protected int getAction(TreeSet<AbstractExecutablePolicy.ActionValueTuple> stateValueSet) {
-        return stateValueSet.isEmpty() ? -1 : Objects.requireNonNull(getMultinomial(1, stateValueSet).pollLast()).action();
+        return stateValueSet.isEmpty() ? -1 : getRandomChoice(stateValueSet);
     }
 
     /**
-     * Returns binomial distribution.
-     * Reference: <a href="https://peterchng.com/blog/2020/10/23/building-binomial-and-multinomial-samplers-in-java/">...</a>
+     * Returns weighted random choice.
      *
-     * @param numberOfTrials number of trials.
-     * @param probability probability.
-     * @return number of successful trials.
-     */
-    private int getBinomial(int numberOfTrials, double probability) {
-        if (numberOfTrials < 1 || probability < 0) return 0;
-        if (probability > 1) return numberOfTrials;
-
-        int numberOfSuccessfulTrials = 0;
-        for (int trial = 0; trial < numberOfTrials; trial++) {
-            if (random.nextDouble() < probability) numberOfSuccessfulTrials++;
-        }
-        return numberOfSuccessfulTrials;
-    }
-
-    /**
-     * Returns multinomial distribution.
-     * Reference: <a href="https://peterchng.com/blog/2020/10/23/building-binomial-and-multinomial-samplers-in-java/">...</a>
-     *
-     * @param numberOfTrials number of trials.
      * @param stateValueSet state value set.
-     * @return multinomial distribution.
+     * @return chosen action.
      */
-    public TreeSet<AbstractExecutablePolicy.ActionValueTuple> getMultinomial(int numberOfTrials, TreeSet<AbstractExecutablePolicy.ActionValueTuple> stateValueSet) {
+    public int getRandomChoice(TreeSet<AbstractExecutablePolicy.ActionValueTuple> stateValueSet) {
+        double valueSum = 0;
+        for (ActionValueTuple actionValueTuple : stateValueSet) valueSum += actionValueTuple.value();
 
-        TreeSet<AbstractExecutablePolicy.ActionValueTuple> result = new TreeSet<>(stateValueSet);
-        result.clear();
-        TreeSet<Integer> actions = new TreeSet<>();
-        for (AbstractExecutablePolicy.ActionValueTuple actionValueTuple : stateValueSet) actions.add(actionValueTuple.action());
-        double sumLeft = 1;
-        int trialsLeft = numberOfTrials;
-        for (Integer action : actions) {
-            ActionValueTuple currentActionValueTuple = null;
-            for (AbstractExecutablePolicy.ActionValueTuple actionValueTuple : stateValueSet) {
-                if (actionValueTuple.action() == action) {
-                    currentActionValueTuple = actionValueTuple;
-                    break;
-                }
-            }
-            if (currentActionValueTuple != null) {
-                if (sumLeft >= 0 && trialsLeft >= 0) {
-                    double probability = currentActionValueTuple.value();
-                    double binomial = getBinomial(trialsLeft, probability / sumLeft);
-                    result.add(new ActionValueTuple(currentActionValueTuple.action(), binomial));
-                    sumLeft -= probability;
-                    trialsLeft -= binomial;
-                }
-                else result.add(new ActionValueTuple(currentActionValueTuple.action(), 0));
-            }
+        double threshold = valueSum * random.nextDouble();
+
+        valueSum = 0;
+        for (ActionValueTuple actionValueTuple : stateValueSet) {
+            valueSum += actionValueTuple.value();
+            if (valueSum >= threshold) return actionValueTuple.action();
         }
 
-        return result;
+        return -1;
     }
 
 }
