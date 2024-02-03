f5544a9ef3ca2b8b9cbeee0dfdcd1ad76b10b816
Simo Aaltonen <simo.aaltonen@kolumbus.fi>
2024-02-03 18:19:05 +0200

    Changed policy handling for DDPG.

--- src/core/reinforcement/policy/updateablepolicy/UpdateableQPolicy.java
+++ src/core/reinforcement/policy/updateablepolicy/UpdateableQPolicy.java
@@ -6,13 +6,14 @@
 package core.reinforcement.policy.updateablepolicy;
 
 import core.network.NeuralNetworkException;
-import core.reinforcement.agent.AgentException;
 import core.reinforcement.agent.State;
 import core.reinforcement.function.FunctionEstimator;
 import core.reinforcement.memory.Memory;
 import core.reinforcement.policy.Policy;
 import core.reinforcement.policy.executablepolicy.ExecutablePolicyType;
+import core.reinforcement.value.QPolicyValueFunction;
 import utils.configurable.DynamicParamException;
+import utils.matrix.Matrix;
 import utils.matrix.MatrixException;
 
 import java.io.IOException;
@@ -24,37 +25,41 @@ import java.io.IOException;
 public class UpdateableQPolicy extends AbstractUpdateablePolicy {
 
     /**
-     * Reference to value function estimator.
+     * Reference to Q policy value function.
      *
      */
-    private final FunctionEstimator valueFunctionEstimator;
+    private QPolicyValueFunction qPolicyValueFunction;
 
     /**
      * Constructor for updateable basic policy.
      *
      * @param executablePolicyType executable policy type.
-     * @param functionEstimator reference to function estimator.
-     * @param valueFunctionEstimator    reference to value function estimator.
+     * @param functionEstimator    reference to function estimator.
+     * @param memory               reference to memory.
+     * @param params               parameters for updateable basic policy.
      * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     * @throws AgentException        throws exception if state action value function is applied to non-updateable policy.
+     * @throws MatrixException throws exception if matrix operation fails.
      */
-    public UpdateableQPolicy(ExecutablePolicyType executablePolicyType, FunctionEstimator functionEstimator, FunctionEstimator valueFunctionEstimator) throws DynamicParamException, AgentException {
-        this(executablePolicyType, functionEstimator, valueFunctionEstimator, null);
+    public UpdateableQPolicy(ExecutablePolicyType executablePolicyType, FunctionEstimator functionEstimator, Memory memory, String params) throws DynamicParamException, MatrixException {
+        super(executablePolicyType, functionEstimator, memory, params);
     }
 
     /**
-     * Constructor for updateable basic policy.
+     * Sets Q policy value function.
      *
-     * @param executablePolicyType executable policy type.
-     * @param functionEstimator reference to function estimator.
-     * @param valueFunctionEstimator reference to value function estimator.
-     * @param params parameters for updateable basic policy.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     * @throws AgentException throws exception if state action value function is applied to non-updateable policy.
+     * @param qPolicyValueFunction Q policy value function.
      */
-    public UpdateableQPolicy(ExecutablePolicyType executablePolicyType, FunctionEstimator functionEstimator, FunctionEstimator valueFunctionEstimator, String params) throws DynamicParamException, AgentException {
-        super(executablePolicyType, functionEstimator, params);
-        this.valueFunctionEstimator = valueFunctionEstimator;
+    public void setQPolicyValueFunction(QPolicyValueFunction qPolicyValueFunction) {
+        this.qPolicyValueFunction = qPolicyValueFunction;
+    }
+
+    /**
+     * Returns Q policy value function.
+     *
+     * @return Q policy value function.
+     */
+    private QPolicyValueFunction getQPolicyValueFunction() {
+        return qPolicyValueFunction;
     }
 
     /**
@@ -67,36 +72,6 @@ public class UpdateableQPolicy extends AbstractUpdateablePolicy {
     /**
      * Returns reference to policy.
      *
-     * @param valueFunctionEstimator reference to value function estimator.
-     * @return reference to policy.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     * @throws AgentException throws exception if state action value function is applied to non-updateable policy.
-     */
-    public Policy reference(FunctionEstimator valueFunctionEstimator) throws DynamicParamException, AgentException {
-        return new UpdateableQPolicy(executablePolicy.getExecutablePolicyType(), getFunctionEstimator(), valueFunctionEstimator, params);
-    }
-
-    /**
-     * Returns reference to policy.
-     *
-     * @param valueFunctionEstimator reference to value function estimator.
-     * @param sharedPolicyFunctionEstimator if true shared policy function estimator is used otherwise new policy function estimator is created.
-     * @param sharedMemory if true shared memory is used between estimators.
-     * @return reference to policy.
-     * @throws IOException throws exception if copying of neural network fails.
-     * @throws ClassNotFoundException throws exception if copying of neural network fails.
-     * @throws MatrixException throws exception if matrix operation fails.
-     * @throws DynamicParamException throws exception if parameter (params) setting fails.
-     * @throws AgentException throws exception if state action value function is applied to non-updateable policy.
-     */
-    public Policy reference(FunctionEstimator valueFunctionEstimator, boolean sharedPolicyFunctionEstimator, boolean sharedMemory) throws DynamicParamException, AgentException, MatrixException, IOException, ClassNotFoundException {
-        return new UpdateableQPolicy(executablePolicy.getExecutablePolicyType(), sharedPolicyFunctionEstimator ? getFunctionEstimator() : getFunctionEstimator().reference(sharedMemory), valueFunctionEstimator, params);
-    }
-
-    /**
-     * Returns reference to policy.
-     *
-     * @param valueFunctionEstimator        reference to value function estimator.
      * @param sharedPolicyFunctionEstimator if true shared policy function estimator is used otherwise new policy function estimator is created.
      * @param memory                        reference to memory.
      * @return reference to policy.
@@ -104,10 +79,22 @@ public class UpdateableQPolicy extends AbstractUpdateablePolicy {
      * @throws ClassNotFoundException throws exception if copying of neural network fails.
      * @throws MatrixException        throws exception if matrix operation fails.
      * @throws DynamicParamException  throws exception if parameter (params) setting fails.
-     * @throws AgentException         throws exception if state action value function is applied to non-updateable policy.
      */
-    public Policy reference(FunctionEstimator valueFunctionEstimator, boolean sharedPolicyFunctionEstimator, Memory memory) throws DynamicParamException, IOException, ClassNotFoundException, AgentException, MatrixException {
-        return new UpdateableQPolicy(executablePolicy.getExecutablePolicyType(), sharedPolicyFunctionEstimator ? getFunctionEstimator() : getFunctionEstimator().reference(memory), valueFunctionEstimator, params);
+    public Policy reference(boolean sharedPolicyFunctionEstimator, Memory memory) throws DynamicParamException, IOException, ClassNotFoundException, MatrixException {
+        return new UpdateableQPolicy(executablePolicy.getExecutablePolicyType(), sharedPolicyFunctionEstimator ? getFunctionEstimator() : getFunctionEstimator().reference(), memory, params);
+    }
+
+    /**
+     * Returns reference to policy.
+     *
+     * @param policyFunctionEstimator reference to policy function estimator.
+     * @param memory                  if true policy will use shared memory otherwise dedicated memory.
+     * @return reference to policy.
+     * @throws MatrixException        throws exception if matrix operation fails.
+     * @throws DynamicParamException  throws exception if parameter (params) setting fails.
+     */
+    public Policy reference(FunctionEstimator policyFunctionEstimator, Memory memory) throws DynamicParamException, MatrixException {
+        return new UpdateableQPolicy(executablePolicy.getExecutablePolicyType(), policyFunctionEstimator, memory, params);
     }
 
     /**
@@ -116,8 +103,8 @@ public class UpdateableQPolicy extends AbstractUpdateablePolicy {
      * @param state state.
      * @return policy gradient value.
      */
-    protected double getPolicyValue(State state) throws MatrixException, NeuralNetworkException {
-        return -valueFunctionEstimator.predictStateActionValues(state).getValue(getFunctionEstimator().predictTargetPolicyValues(state).argmax()[0], 0, 0);
+    protected Matrix getPolicyGradient(State state) throws MatrixException, NeuralNetworkException {
+        return state.policyValues.multiply(getQPolicyValueFunction().getTargetValues(state, true));
     }
 
 }
